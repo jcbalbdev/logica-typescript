2fe6069235ad71ec07f1fb1ba4573e1aea30085b
jcbalbdev <jcbalbdev@gmail.com>
2025-04-15 18:11:46 -0500

    Actualización automática: 2025-04-15 18:11:46

--- lv1-es-mayor-de-edad.ts
+++ lv1-es-mayor-de-edad.ts
@@ -1,1 +1,9 @@
+/* Pide la edad de una persona (simulada con una variable) y muestra si es mayor o menor de edad usando if. */
 
+let tuEdad: number = 12;
+
+if (tuEdad >=18) {
+  console.log("Es mayor de edad");
+} else {
+  console.log("Es menor de edad");
+}
