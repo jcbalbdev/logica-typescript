04c1d3d46802b7926b5a2aad2c1c1face9931d10
jcbalbdev <jcbalbdev@gmail.com>
2025-04-15 18:49:32 -0500

    Actualización automática: 2025-04-15 18:49:32

--- lv4-login-simple.js
+++ lv4-login-simple.js
@@ -1,1 +1,16 @@
 "use strict";
+/* Simula un sistema de login con usuario y contraseña correctos usando if. */
+let login = {
+    usuario: "pedrito",
+    pass: "la contraseña mas segura del mundo"
+};
+const { usuario, pass } = login;
+function verificandoDatos(user, pass) {
+    if ((user === "pedrito") && (pass === "la contraseña mas segura del mundo")) {
+        return `se permitio la entrada de  ${user}`;
+    }
+    else {
+        return `no se permitio la entrada de  ${user}, usuario no valido`;
+    }
+}
+console.log(verificandoDatos(usuario, pass));
--- lv4-login-simple.ts
+++ lv4-login-simple.ts
@@ -1,1 +1,23 @@
+/* Simula un sistema de login con usuario y contraseña correctos usando if. */
 
+interface Login{
+  usuario:string;
+  pass:string;
+}
+
+let login: Login = {
+  usuario: "pedrito",
+  pass:"la contraseña mas segura del mundo"
+}
+
+const { usuario,pass} = login;
+
+function verificandoDatos(user: string,pass: string): string{
+  if((user === "pedrito") && (pass === "la contraseña mas segura del mundo")){
+    return `se permitio la entrada de  ${user}`;
+  }else{
+    return `no se permitio la entrada de  ${user}, usuario no valido`;
+  }
+}
+
+console.log(verificandoDatos(usuario,pass));
