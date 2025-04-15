1abff53270bb71ffde32654500e7a2af2c5b61f3
jcbalbdev <jcbalbdev@gmail.com>
2025-04-15 18:30:00 -0500

    Actualización automática: 2025-04-15 18:30:00

--- lv2-numero-positivo-negativo-o-cero.js
+++ lv2-numero-positivo-negativo-o-cero.js
@@ -1,1 +1,16 @@
 "use strict";
+/* Evalúa una variable numérica y muestra si es positiva, negativa o igual a cero. */
+let veintiuno = 21;
+let numeroCero = 0;
+let negativo = -21;
+function queEs(valor) {
+    if (valor > 0) {
+        return `el numero ${valor} es positivo`;
+    }
+    else if (valor < 0) {
+        return `el numero ${valor} es negativo`;
+    }
+    else {
+        return `el numero ${valor} es neutro`;
+    }
+}
