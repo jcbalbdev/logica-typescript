2d20116595b202ea95f8e56125ebe475a5d75195
jcbalbdev <jcbalbdev@gmail.com>
2025-04-16 12:19:44 -0500

    Actualización automática: 2025-04-16 12:19:44

--- lv6-condiciones-combinadas-complejas.js
+++ lv6-condiciones-combinadas-complejas.js
@@ -1,19 +1,27 @@
 "use strict";
 /* Dado el rol de un usuario (admin, editor, visitante) y si está logueado, muestra los permisos. Usa condiciones anidadas y lógicas. */
+var Rol;
+(function (Rol) {
+    Rol["Administrador"] = "admin";
+    Rol["Editor"] = "editor";
+    Rol["Visitante"] = "visitante";
+})(Rol || (Rol = {}));
 let pedrito = {
-    rol: "admin",
+    rol: Rol.Administrador,
     logueado: true
 };
 let pascal = {
-    rol: "editor",
+    rol: Rol.Editor,
     logueado: true
 };
 let barry = {
-    rol: "editor",
-    logueado: false
+    rol: Rol.Visitante,
+    logueado: true
 };
+const rolesPermitidos = [Rol.Admin, Rol.Editor, Rol.Visitante];
+;
 function evaluandoPermiso({ rol, logueado }) {
-    if (((rol === "admin") || (rol === "editor") || (rol === "visitante")) && (logueado === true)) {
+    if ((rolesPermitidos.includes(rol)) && (logueado === true)) {
         return "acceso permitido";
     }
     else {
--- lv6-condiciones-combinadas-complejas.ts
+++ lv6-condiciones-combinadas-complejas.ts
@@ -1,29 +1,37 @@
 /* Dado el rol de un usuario (admin, editor, visitante) y si está logueado, muestra los permisos. Usa condiciones anidadas y lógicas. */
+enum Rol {
+  Administrador = "admin",
+  Editor = "editor",
+  Visitante = "visitante"
+}
+
 
 interface User {
-  rol:string;
+  rol:Rol;
   logueado:boolean;
 }
 
 
 let pedrito: User = {
-  rol:"admin",
+  rol:Rol.Administrador,
   logueado:true
 }
 
 let pascal: User = {
-  rol:"editor",
+  rol:Rol.Editor,
   logueado:true
 }
 
 let barry: User = {
-  rol:"editor",
-  logueado:false
+  rol:Rol.Visitante,
+  logueado:true
 }
+
+const rolesPermitidos: Rol[] = [Rol.Admin, Rol.Editor, Rol.Visitante];;
 
 function evaluandoPermiso({rol,logueado}: User): string{
   
-  if(((rol === "admin") || (rol === "editor") || (rol === "visitante")) && (logueado === true)){
+  if((rolesPermitidos.includes(rol)) && (logueado === true)){
     return "acceso permitido";
   }else{
     return "acceso denegado";
