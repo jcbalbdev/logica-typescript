e0879ad3526dd738893cdc545a0cc6ae4b0c7f7e
jcbalbdev <jcbalbdev@gmail.com>
2025-04-16 11:59:35 -0500

    Actualización automática: 2025-04-16 11:59:35

--- lv6-condiciones-combinadas-complejas.js
+++ lv6-condiciones-combinadas-complejas.js
@@ -1,1 +1,11 @@
 "use strict";
+/* Dado el rol de un usuario (admin, editor, visitante) y si está logueado, muestra los permisos. Usa condiciones anidadas y lógicas. */
+let pedrito = {
+    rol: "admin",
+    logueado: true
+};
+function evaluandoPermiso({ rol, logueado }) {
+    let respuesta = ((rol === "admin") && (logueado === true)) ? "pase permitido" : "pase denegado";
+    return respuesta;
+}
+console.log(evaluandoPermiso(pedrito));
--- lv6-condiciones-combinadas-complejas.ts
+++ lv6-condiciones-combinadas-complejas.ts
@@ -1,1 +1,20 @@
+/* Dado el rol de un usuario (admin, editor, visitante) y si está logueado, muestra los permisos. Usa condiciones anidadas y lógicas. */
 
+interface User {
+  rol:string;
+  logueado:boolean;
+}
+
+
+let pedrito: User = {
+  rol:"admin",
+  logueado:true
+}
+
+function evaluandoPermiso({rol,logueado}: User): string{
+  let respuesta: string = ((rol === "admin") && (logueado === true))?"pase permitido":"pase denegado";
+
+  return respuesta;
+}
+
+console.log(evaluandoPermiso(pedrito));
