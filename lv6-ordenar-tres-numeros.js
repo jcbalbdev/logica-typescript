9aa96108c54c0c666b0f928faadab9f63d2adffc
jcbalbdev <jcbalbdev@gmail.com>
2025-04-15 20:31:56 -0500

    Actualización automática: 2025-04-15 20:31:56

--- lv6-ordenar-tres-numeros.js
+++ lv6-ordenar-tres-numeros.js
@@ -1,34 +1,45 @@
 "use strict";
 /* Dado tres números, ordénalos de menor a mayor sin usar arrays ni métodos de ordenamiento. */
-let p = 20;
+let p = 0;
 let s = -10;
 let t = 3;
 function menoramayor(a, b, c) {
-    if ((a > b) && (a > c)) {
-        if (b > c) {
-            return `el orden de ${a}, ${b} y ${c} :  ${c} ${b} ${a}`;
+    let primero;
+    let segundo;
+    let tercero;
+    if (a <= b && a <= c) {
+        primero = a;
+        if (b <= c) {
+            segundo = b;
+            tercero = c;
         }
         else {
-            return `el orden de ${a}, ${b} y ${c} :  ${b} ${c} ${a}`;
+            segundo = c;
+            tercero = b;
+        }
+    }
+    else if (b <= a && b <= c) {
+        primero = b;
+        if (a <= c) {
+            segundo = a;
+            tercero = c;
+        }
+        else {
+            segundo = c;
+            tercero = a;
         }
     }
     else {
-        if (b > c) {
-            if (c > a) {
-                return `el orden de ${a}, ${b} y ${c} :  ${a} ${c} ${b}`;
-            }
-            else {
-                return `el orden de ${a}, ${b} y ${c} :  ${c} ${a} ${b}`;
-            }
+        primero = c;
+        if (a <= b) {
+            segundo = a;
+            tercero = b;
         }
         else {
-            if (b > a) {
-                return `el orden de ${a}, ${b} y ${c} :  ${a} ${b} ${c}`;
-            }
-            else {
-                return `el orden de ${a}, ${b} y ${c} :  ${b} ${a} ${c}`;
-            }
+            segundo = b;
+            tercero = a;
         }
     }
+    return `El orden de ${a}, ${b} y ${c} es: ${primero} ${segundo} ${tercero}`;
 }
 console.log(menoramayor(p, s, t));
--- lv6-ordenar-tres-numeros.ts
+++ lv6-ordenar-tres-numeros.ts
@@ -1,32 +1,43 @@
 /* Dado tres números, ordénalos de menor a mayor sin usar arrays ni métodos de ordenamiento. */
 
-let p: number = 20;
+let p: number = 0;
 let s: number = -10;
 let t: number = 3;
 
 function menoramayor(a: number,b: number,c: number): string{
-
-  if((a>b) && (a>c)){
-    if(b>c){
-      return `el orden de ${a}, ${b} y ${c} :  ${c} ${b} ${a}`;
+  let primero: number;
+  let segundo: number;
+  let tercero: number;
+  if (a <=b && a<=c) {
+    primero = a;
+    if(b<=c){
+      segundo=b;
+      tercero=c;
     }else{
-      return `el orden de ${a}, ${b} y ${c} :  ${b} ${c} ${a}`;
+      segundo=c;
+      tercero=b;
     }
-  }else{
-    if(b>c){
-      if(c>a){
-        return `el orden de ${a}, ${b} y ${c} :  ${a} ${c} ${b}`;
-      }else{
-        return `el orden de ${a}, ${b} y ${c} :  ${c} ${a} ${b}`;
-      }
-    }else{
-      if(b>a){
-        return `el orden de ${a}, ${b} y ${c} :  ${a} ${b} ${c}`;
-      }else{
-        return `el orden de ${a}, ${b} y ${c} :  ${b} ${a} ${c}`;
-      }
+  }else if (b <= a && b <= c) {
+    primero = b;
+    if (a <= c) {
+      segundo = a;
+      tercero = c;
+    } else {
+      segundo = c;
+      tercero = a;
+    }
+  } else {
+    primero = c;
+    if (a <= b) {
+      segundo = a;
+      tercero = b;
+    } else {
+      segundo = b;
+      tercero = a;
     }
   }
+
+  return `El orden de ${a}, ${b} y ${c} es: ${primero} ${segundo} ${tercero}`;
 }
 
 console.log(menoramayor(p,s,t));
