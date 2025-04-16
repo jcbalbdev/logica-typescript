cd781512b29cd4beb71b1e99d75fc5281eb26546
jcbalbdev <jcbalbdev@gmail.com>
2025-04-15 19:20:11 -0500

    Actualización automática: 2025-04-15 19:20:10

--- lv3-switch-estacion-del-año.ts
+++ lv3-switch-estacion-del-año.ts
@@ -1,1 +1,20 @@
+/* Usa switch para mostrar la estación según el número del mes. */
+let estacion: string = "primavera";
+
+switch (estacion) {
+  case "primavera":
+    console.log("es primavera");
+    break;
+  case "verano":
+    console.log("es verano");
+    break;
+  case "otoño":
+    console.log("es otoño");
+  break;
+  case "invierno":
+    console.log("es invierno");
+  default:
+    console.log("es un dia normal");
+    break;
+}
 
