1207c5d04e7988424ec0d79a2c1f1ba40c376c65
jcbalbdev <jcbalbdev@gmail.com>
2025-04-15 19:25:16 -0500

    Actualización automática: 2025-04-15 19:25:16

--- lv4-switch-con-multiples-casos.ts
+++ lv4-switch-con-multiples-casos.ts
@@ -1,1 +1,20 @@
+/* Escribe un programa que evalúe una nota (A, B, C, etc.) y dé un mensaje. Agrupa varios casos que tengan el mismo mensaje. */
 
+let nota: string = "B";
+
+switch (nota){
+  case "A":
+  case "B":
+    console.log("esta aprobado");
+  break;
+  case "C":
+  case "D":
+    console.log("esta en el promedio");
+  break;
+  case "E":
+    console.log("esta desaprobado");
+  break;
+  default:
+    console.log("repetiste el curso");
+  break;
+}
