62aee785a4bc9472480a0ed5d42d5cbba1f1ffcd
jcbalbdev <jcbalbdev@gmail.com>
2025-04-16 11:33:13 -0500

    Actualización automática: 2025-04-16 11:33:13

--- lv3-uso-de-ternario-simple.ts
+++ lv3-uso-de-ternario-simple.ts
@@ -1,1 +1,11 @@
+/* Declara una variable esPremium: boolean y usa el operador ternario para mostrar un mensaje diferente según su valor. */
 
+let esPremium: boolean = true;
+
+function mensaje(valor: boolean): string{
+  let respuesta: string = valor? "es true" : "es false";
+  
+  return respuesta;
+}
+
+console.log(mensaje(esPremium));
