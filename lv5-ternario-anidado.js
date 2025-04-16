9dd2673f8c0a0252f93d1b8228d5cf92ce174aca
jcbalbdev <jcbalbdev@gmail.com>
2025-04-16 11:43:25 -0500

    Actualización automática: 2025-04-16 11:43:25

--- lv5-ternario-anidado.js
+++ lv5-ternario-anidado.js
@@ -1,6 +1,6 @@
 "use strict";
 /* Evalúa la edad de una persona y clasifica como niño, joven, adulto o anciano usando ternarios anidados. */
-let suEdad = 23;
+let suEdad = 12;
 function evaluaEdad(edad) {
     let respuesta = edad > 60 ? "es anciano" : ((edad >= 18) && (edad <= 60)) ? "es adulto" : ((edad >= 13) && (edad < 18)) ? "es joven" : edad < 13 ? "es niño" : "la edad no entra en ningun rango";
     return respuesta;
--- lv5-ternario-anidado.ts
+++ lv5-ternario-anidado.ts
@@ -1,6 +1,6 @@
 /* Evalúa la edad de una persona y clasifica como niño, joven, adulto o anciano usando ternarios anidados. */
 
-let suEdad: number = 23;
+let suEdad: number = 12;
 
 function evaluaEdad(edad: number): string{
   let respuesta: string = edad > 60? "es anciano": ((edad>=18)&&(edad<=60))? "es adulto": ((edad>=13)&&(edad<18))?"es joven":edad <13 ? "es niño": "la edad no entra en ningun rango";
