d886b424cf4bdf517a15fc981c5ff65ebf7e964e
jcbalbdev <jcbalbdev@gmail.com>
2025-04-15 19:41:24 -0500

    Actualización automática: 2025-04-15 19:41:24

--- lv5-menor-de-tres-numeros.js
+++ lv5-menor-de-tres-numeros.js
@@ -1,1 +1,23 @@
 "use strict";
+/* Dado tres números, encuentra el menor usando if. */
+let prim = 23;
+let seg = 12;
+let terc = 32;
+function elMenorEs(a, b, c) {
+    if (a <= b) {
+        if (b < c) {
+            return `el numero ${c} es el mayor`;
+        }
+        else {
+            return `el numero ${b} es el mayor`;
+        }
+    }
+    else {
+        if (a < c) {
+            return `el numero ${c} es el mayor`;
+        }
+        else {
+            return `el numero ${a} es el mayor`;
+        }
+    }
+}
--- lv5-menor-de-tres-numeros.ts
+++ lv5-menor-de-tres-numeros.ts
@@ -1,1 +1,22 @@
+/* Dado tres números, encuentra el menor usando if. */
 
+let prim: number = 23;
+let seg: number = 12;
+let terc: number = 32;
+
+function elMenorEs(a: number,b: number,c: number): string{
+  if (a<=b) {
+    if(b<c){
+      return `el numero ${c} es el mayor`;
+    }else{
+      return `el numero ${b} es el mayor`;
+    }
+  }else{
+    if(a<c){
+      return `el numero ${c} es el mayor`;
+    }else{
+      return `el numero ${a} es el mayor`;
+    }
+  }
+
+}
