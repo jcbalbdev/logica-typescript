d7d3e6dd55c00befb300b9ceff565d0e9a865c5e
jcbalbdev <jcbalbdev@gmail.com>
2025-04-15 20:19:45 -0500

    Actualización automática: 2025-04-15 20:19:44

--- lv6-ordenar-tres-numeros.js
+++ lv6-ordenar-tres-numeros.js
@@ -1,1 +1,35 @@
 "use strict";
+/* Dado tres números, ordénalos de menor a mayor sin usar arrays ni métodos de ordenamiento. */
+let p = 2;
+let s = 10;
+let t = 3;
+function menoramayor(a, b, c) {
+    let mayor = a;
+    if (mayor) {
+        if (b > c) {
+            return `el orden de ${a}, ${b} y ${c} :  ${a} ${b} ${c}`;
+        }
+        else {
+            return `el orden de ${a}, ${b} y ${c} :  ${a} ${c} ${b}`;
+        }
+    }
+    else {
+        if (b > c) {
+            if (c > a) {
+                return `el orden de ${a}, ${b} y ${c} :  ${b} ${c} ${a}`;
+            }
+            else {
+                return `el orden de ${a}, ${b} y ${c} :  ${b} ${a} ${c}`;
+            }
+        }
+        else {
+            if (b > a) {
+                return `el orden de ${a}, ${b} y ${c} :  ${c} ${b} ${a}`;
+            }
+            else {
+                return `el orden de ${a}, ${b} y ${c} :  ${c} ${a} ${b}`;
+            }
+        }
+    }
+}
+console.log(menoramayor(p, s, t));
--- lv6-ordenar-tres-numeros.ts
+++ lv6-ordenar-tres-numeros.ts
@@ -1,1 +1,33 @@
+/* Dado tres números, ordénalos de menor a mayor sin usar arrays ni métodos de ordenamiento. */
 
+let p: number = 2;
+let s: number = 10;
+let t: number = 3;
+
+function menoramayor(a: number,b: number,c: number): string{
+  let mayor= a;
+
+  if(mayor){
+    if(b>c){
+      return `el orden de ${a}, ${b} y ${c} :  ${a} ${b} ${c}`;
+    }else{
+      return `el orden de ${a}, ${b} y ${c} :  ${a} ${c} ${b}`;
+    }
+  }else{
+    if(b>c){
+      if(c>a){
+        return `el orden de ${a}, ${b} y ${c} :  ${b} ${c} ${a}`;
+      }else{
+        return `el orden de ${a}, ${b} y ${c} :  ${b} ${a} ${c}`;
+      }
+    }else{
+      if(b>a){
+        return `el orden de ${a}, ${b} y ${c} :  ${c} ${b} ${a}`;
+      }else{
+        return `el orden de ${a}, ${b} y ${c} :  ${c} ${a} ${b}`;
+      }
+    }
+  }
+}
+
+console.log(menoramayor(p,s,t));
