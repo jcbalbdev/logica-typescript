23d105f61874ef6397492779e0e679f0db9ecf6a
jcbalbdev <jcbalbdev@gmail.com>
2025-04-15 20:22:50 -0500

    Actualización automática: 2025-04-15 20:22:50

--- lv6-ordenar-tres-numeros.js
+++ lv6-ordenar-tres-numeros.js
@@ -1,32 +1,32 @@
 "use strict";
 /* Dado tres números, ordénalos de menor a mayor sin usar arrays ni métodos de ordenamiento. */
-let p = 2;
-let s = 10;
+let p = 20;
+let s = -10;
 let t = 3;
 function menoramayor(a, b, c) {
     if ((a > b) && (a > c)) {
         if (b > c) {
-            return `el orden de ${a}, ${b} y ${c} :  ${a} ${b} ${c}`;
+            return `el orden de ${a}, ${b} y ${c} :  ${c} ${b} ${a}`;
         }
         else {
-            return `el orden de ${a}, ${b} y ${c} :  ${a} ${c} ${b}`;
+            return `el orden de ${a}, ${b} y ${c} :  ${b} ${c} ${a}`;
         }
     }
     else {
         if (b > c) {
             if (c > a) {
-                return `el orden de ${a}, ${b} y ${c} :  ${b} ${c} ${a}`;
+                return `el orden de ${a}, ${b} y ${c} :  ${a} ${c} ${b}`;
             }
             else {
-                return `el orden de ${a}, ${b} y ${c} :  ${b} ${a} ${c}`;
+                return `el orden de ${a}, ${b} y ${c} :  ${c} ${a} ${b}`;
             }
         }
         else {
             if (b > a) {
-                return `el orden de ${a}, ${b} y ${c} :  ${c} ${b} ${a}`;
+                return `el orden de ${a}, ${b} y ${c} :  ${a} ${b} ${c}`;
             }
             else {
-                return `el orden de ${a}, ${b} y ${c} :  ${c} ${a} ${b}`;
+                return `el orden de ${a}, ${b} y ${c} :  ${b} ${a} ${c}`;
             }
         }
     }
--- lv6-ordenar-tres-numeros.ts
+++ lv6-ordenar-tres-numeros.ts
@@ -1,7 +1,7 @@
 /* Dado tres números, ordénalos de menor a mayor sin usar arrays ni métodos de ordenamiento. */
 
-let p: number = 2;
-let s: number = 10;
+let p: number = 20;
+let s: number = -10;
 let t: number = 3;
 
 function menoramayor(a: number,b: number,c: number): string{
@@ -9,22 +9,22 @@ function menoramayor(a: number,b: number,c: number): string{
 
   if((a>b) && (a>c)){
     if(b>c){
-      return `el orden de ${a}, ${b} y ${c} :  ${a} ${b} ${c}`;
+      return `el orden de ${a}, ${b} y ${c} :  ${c} ${b} ${a}`;
     }else{
-      return `el orden de ${a}, ${b} y ${c} :  ${a} ${c} ${b}`;
+      return `el orden de ${a}, ${b} y ${c} :  ${b} ${c} ${a}`;
     }
   }else{
     if(b>c){
       if(c>a){
-        return `el orden de ${a}, ${b} y ${c} :  ${b} ${c} ${a}`;
+        return `el orden de ${a}, ${b} y ${c} :  ${a} ${c} ${b}`;
       }else{
-        return `el orden de ${a}, ${b} y ${c} :  ${b} ${a} ${c}`;
+        return `el orden de ${a}, ${b} y ${c} :  ${c} ${a} ${b}`;
       }
     }else{
       if(b>a){
-        return `el orden de ${a}, ${b} y ${c} :  ${c} ${b} ${a}`;
+        return `el orden de ${a}, ${b} y ${c} :  ${a} ${b} ${c}`;
       }else{
-        return `el orden de ${a}, ${b} y ${c} :  ${c} ${a} ${b}`;
+        return `el orden de ${a}, ${b} y ${c} :  ${b} ${a} ${c}`;
       }
     }
   }
