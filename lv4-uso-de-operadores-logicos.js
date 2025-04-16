05ca26fea7fb6af2018dcf12fea1b645baf65943
jcbalbdev <jcbalbdev@gmail.com>
2025-04-15 19:08:59 -0500

    Actualización automática: 2025-04-15 19:08:58

--- lv4-uso-de-operadores-logicos.js
+++ lv4-uso-de-operadores-logicos.js
@@ -1,1 +1,14 @@
 "use strict";
+/* Simula un sistema de seguridad: acceso permitido solo si tiene tarjeta y no está bloqueado. */
+let empleado = {
+    tarjeta: true,
+    bloqueado: false
+};
+function acceso(tarjeta, estaBloqueado) {
+    if ((tarjeta === true) && (!estaBloqueado === true)) {
+        return "el empleado tiene acceso";
+    }
+    else {
+        return "el empleado no tiene acceso";
+    }
+}
--- lv4-uso-de-operadores-logicos.ts
+++ lv4-uso-de-operadores-logicos.ts
@@ -1,1 +1,20 @@
+/* Simula un sistema de seguridad: acceso permitido solo si tiene tarjeta y no está bloqueado. */
+
+interface Trabajador{
+  tarjeta:boolean;
+  bloqueado:boolean;
+}
+
+let empleado: Trabajador ={
+  tarjeta:true,
+  bloqueado:false
+}
+
+function acceso(tarjeta: boolean,estaBloqueado: boolean): string{
+  if ((tarjeta === true) && (!estaBloqueado === true)) {
+    return "el empleado tiene acceso";
+  } else {
+    return "el empleado no tiene acceso";
+  }
+}
 
