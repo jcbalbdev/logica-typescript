eea4d6b6147e4c6b03ece88147769c1e6332de21
jcbalbdev <jcbalbdev@gmail.com>
2025-04-17 12:03:30 -0500

    Actualización automática: 2025-04-17 12:03:30

--- lv5-evaluador-de-año-bisiesto.js
+++ lv5-evaluador-de-año-bisiesto.js
@@ -1,1 +1,11 @@
 "use strict";
+/* Dado un año, determina si es bisiesto: divisible por 4, pero no por 100, excepto si también es divisible por 400. */
+let año = 1993;
+function esBisiesto(año) {
+    if (año % 4 === 0) {
+        return "Es bisiesto";
+    }
+    else {
+        return "No es bisiesto";
+    }
+}
