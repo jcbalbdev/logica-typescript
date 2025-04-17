03d0698b6c231cc5d39faea6f5687e70427ee649
jcbalbdev <jcbalbdev@gmail.com>
2025-04-16 23:46:14 -0500

    Actualización automática: 2025-04-16 23:46:13

--- lv5-calculadora-con-switch.js
+++ lv5-calculadora-con-switch.js
@@ -1,18 +1,17 @@
 "use strict";
 /* Crea una mini calculadora que reciba dos números y una operación (+, -, *, /) y devuelva el resultado. */
-var Operaciones;
-(function (Operaciones) {
-    Operaciones["Suma"] = "+";
-    Operaciones["Resta"] = "-";
-    Operaciones["Producto"] = "*";
-    Operaciones["Division"] = "/";
-})(Operaciones || (Operaciones = {}));
-const operación = [Operaciones.Suma, Operaciones.Resta, Operaciones.Producto, Operaciones.Division];
+var Operacione;
+(function (Operacione) {
+    Operacione["Suma"] = "+";
+    Operacione["Resta"] = "-";
+    Operacione["Producto"] = "*";
+    Operacione["Division"] = "/";
+})(Operacione || (Operacione = {}));
 let numPrim = 12;
 let numSeg = 4;
 function calculadora(a, b, op) {
     switch (op) {
-        case operacion[0]:
+        case Operacione.Suma:
             return `la suma de ${a} y ${b} es ${a + b}`;
             break;
         default:
--- lv5-calculadora-con-switch.ts
+++ lv5-calculadora-con-switch.ts
@@ -1,13 +1,11 @@
 /* Crea una mini calculadora que reciba dos números y una operación (+, -, *, /) y devuelva el resultado. */
 
-enum Operaciones {
+enum Operacione {
   Suma = "+",
   Resta = "-",
   Producto = "*",
   Division = "/"
 }
-
-const operación: Operaciones[] = [Operaciones.Suma,Operaciones.Resta,Operaciones.Producto,Operaciones.Division]; 
 
 let numPrim: number = 12;
 let numSeg: number =4;
@@ -15,7 +13,7 @@ let numSeg: number =4;
 
 function calculadora(a: number,b: number,op: string): string{
   switch (op) {
-    case operacion[0]:
+    case Operacione.Suma:
       return `la suma de ${a} y ${b} es ${a+b}`;
       break;
   
