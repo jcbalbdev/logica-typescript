7d9ec6724ec890e4a03d7a58310d9798471b712c
jcbalbdev <jcbalbdev@gmail.com>
2025-04-17 12:35:47 -0500

    Actualización automática: 2025-04-17 12:35:47

--- lv4-simulador-de-semaforo.js
+++ lv4-simulador-de-semaforo.js
@@ -1,1 +1,23 @@
 "use strict";
+/* Según el color (rojo, amarillo, verde), muestra qué hacer: "Detente", "Precaución", "Avanza". */
+var Color;
+(function (Color) {
+    Color["Rojo"] = "Detente";
+    Color["Amarillo"] = "Precauci\u00F3n";
+    Color["Verde"] = "Avanza";
+})(Color || (Color = {}));
+function semaforoEn(color) {
+    if (color === Color.Rojo) {
+        return `el semaforo esta en rojo asi que ${Color.Rojo}`;
+    }
+    else if (color === Color.Amarillo) {
+        return `el semaforo esta en amarillo asi que ${Color.Amarillo}`;
+    }
+    else if (color === Color.Verde) {
+        return `el semaforo esta en verde asi que ${Color.Verde}`;
+    }
+    else {
+        return `el semaforo esta apagado`;
+    }
+}
+console.log(semaforoEn(Color.Amarillo));
--- lv4-simulador-de-semaforo.ts
+++ lv4-simulador-de-semaforo.ts
@@ -1,1 +1,22 @@
+/* Según el color (rojo, amarillo, verde), muestra qué hacer: "Detente", "Precaución", "Avanza". */
+
+enum Color {
+  Rojo = "Detente",
+  Amarillo = "Precaución",
+  Verde = "Avanza"
+}
+
+function semaforoEn(color: Color): string{
+  if (color === Color.Rojo) {
+    return `el semaforo esta en rojo asi que ${Color.Rojo}`;
+  } else if(color === Color.Amarillo){
+    return `el semaforo esta en amarillo asi que ${Color.Amarillo}`;
+  } else if(color === Color.Verde){
+    return `el semaforo esta en verde asi que ${Color.Verde}`;
+  }else{
+    return `el semaforo esta apagado`;
+  }
+}
+
+console.log(semaforoEn(Color.Amarillo));
 
