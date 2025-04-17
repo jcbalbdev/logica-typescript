0151b60d11c8e87970bb318245319ccc64c1e365
jcbalbdev <jcbalbdev@gmail.com>
2025-04-17 12:49:56 -0500

    Actualización automática: 2025-04-17 12:49:56

--- lv5-validacion-de-input.ts
+++ lv5-validacion-de-input.ts
@@ -1,1 +1,17 @@
+/* Simula una entrada del usuario para ingresar un número. Valida si es realmente un número y muestra un error si no lo es. */
 
+let codigo: number | string = "34";
+
+function evaluandoCodigo(codigo: number | string): string{
+  let tipo = typeof(codigo);
+  switch (tipo) {
+    case "number" :
+      return `es de tipo number`;
+    case "string" :
+      return `es de tipo string`;
+    default:
+      return `tipo invalido`;
+  }
+}
+
+console.log(evaluandoCodigo(codigo));
