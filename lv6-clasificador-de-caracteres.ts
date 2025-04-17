e986606fc161c474f2a865869f84258d825b8e7e
jcbalbdev <jcbalbdev@gmail.com>
2025-04-17 16:11:32 -0500

    Actualización automática: 2025-04-17 16:11:32

--- lv6-clasificador-de-caracteres.ts
+++ lv6-clasificador-de-caracteres.ts
@@ -1,1 +1,18 @@
+/* Dado un carácter, evalúa si es una letra, número o símbolo. */
 
+let caracter: string =  "G";
+
+function evaluandoCaracter(caracter: string): string{
+  let numero: number = Number(caracter);
+  let codigoAscii: number = caracter.charCodeAt(0);
+
+  if(!isNaN(numero)){
+    return `el caracter ${caracter} es un numero`;
+  }else if((codigoAscii >= 65 && codigoAscii <= 90) || (codigoAscii >= 97 && codigoAscii <= 122)){
+    return `el caracter ${caracter} es una letra`;
+  }else{
+    return `el caracter ${caracter} es un simbolo`;
+  }
+}
+
+console.log(evaluandoCaracter(caracter));
