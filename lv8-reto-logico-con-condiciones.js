5fc9716f746b3053ff1ffc9d2ce8777ed0f49546
jcbalbdev <jcbalbdev@gmail.com>
2025-04-19 16:38:18 -0500

    Actualización automática: 2025-04-19 16:38:18

--- lv8-reto-logico-con-condiciones.js
+++ lv8-reto-logico-con-condiciones.js
@@ -1,8 +1,8 @@
 "use strict";
 /* Evalúa tres números enteros. Si alguno es igual a la suma de los otros dos, devuelve true, si no, false. */
-let np = 2;
-let ns = 3;
-let nt = 4;
+let np = 0;
+let ns = 10;
+let nt = 10;
 function comparando(pv, sv, tv) {
     if ((pv - (sv + tv) === 0) || (sv - (pv + tv) === 0) || (tv - (sv + pv) === 0)) {
         return true;
