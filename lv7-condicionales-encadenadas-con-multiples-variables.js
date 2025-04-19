255fdf91ad2039d3129a77dddac59ad8eca18a3a
jcbalbdev <jcbalbdev@gmail.com>
2025-04-19 16:59:29 -0500

    Actualización automática: 2025-04-19 16:59:29

--- lv7-condicionales-encadenadas-con-multiples-variables.js
+++ lv7-condicionales-encadenadas-con-multiples-variables.js
@@ -1,1 +1,17 @@
 "use strict";
+/* Si una persona tiene más de 18 años, un carnet válido y no tiene multas, puede conducir. Evalúa con if. */
+function permisoDeConducir({ edadPersona, carnetValido, tieneMultas }) {
+    let condicion = ((edadPersona > 18) || (carnetValido === true) || (tieneMultas === false));
+    if (condicion) {
+        return `la persona puede conducir`;
+    }
+    else {
+        return `la persona no puede conducir`;
+    }
+}
+let batman = {
+    edadPersona: 21,
+    carnetValido: true,
+    tieneMultas: false
+};
+console.log(permisoDeConducir(batman));
--- lv7-condicionales-encadenadas-con-multiples-variables.ts
+++ lv7-condicionales-encadenadas-con-multiples-variables.ts
@@ -1,1 +1,27 @@
+/* Si una persona tiene más de 18 años, un carnet válido y no tiene multas, puede conducir. Evalúa con if. */
 
+interface Conductor{
+  edadPersona:number;
+  carnetValido:boolean;
+  tieneMultas:boolean;
+}
+
+function permisoDeConducir({edadPersona,carnetValido,tieneMultas}: Conductor): string{
+  
+  let condicion: boolean = ((edadPersona > 18) || (carnetValido === true) || (tieneMultas === false));
+  
+  if(condicion){
+    return `la persona puede conducir`;
+  }else{
+    return `la persona no puede conducir`;
+  }
+}
+
+let batman: Conductor = {
+  edadPersona:21,
+  carnetValido:true,
+  tieneMultas:false
+}
+
+
+console.log(permisoDeConducir(batman));
