b46d2e54c53b74721e562d5f2dfdd94086b34a89
jcbalbdev <jcbalbdev@gmail.com>
2025-04-19 14:58:47 -0500

    Actualización automática: 2025-04-19 14:58:47

--- lv7-juego-piedra-papel-o-tijera.js
+++ lv7-juego-piedra-papel-o-tijera.js
@@ -4,6 +4,41 @@
 let pedroPascal = 1;
 let cpu = Math.floor(Math.random() * 3) + 1;
 function janken(playerUno, playerDos) {
-    return `${playerUno} y ${playerDos}`;
+    if (playerUno === 1) {
+        if (playerDos === 2) {
+            return `el jugador ${playerUno} escogio piedra y el jugador ${playerDos} escogio tijera. GANADOR: ${playerUno}`;
+        }
+        else if (playerDos === 3) {
+            return `el jugador ${playerUno} escogio piedra y el jugador ${playerDos} escogio papel. GANADOR: ${playerDos}`;
+        }
+        else {
+            return `el jugador ${playerUno} escogio piedra y el jugador ${playerDos} escogio piedra. GANADOR: EMPATE!!`;
+        }
+    }
+    else if (playerUno === 2) {
+        if (playerDos === 2) {
+            return `el jugador ${playerUno} escogio tijera y el jugador ${playerDos} escogio tijera. GANADOR: EMPATE!!`;
+        }
+        else if (playerDos === 3) {
+            return `el jugador ${playerUno} escogio tijera y el jugador ${playerDos} escogio papel. GANADOR: ${playerUno}`;
+        }
+        else {
+            return `el jugador ${playerUno} escogio tijera y el jugador ${playerDos} escogio papel. GANADOR: ${playerDos}`;
+        }
+    }
+    else if (playerUno === 3) {
+        if (playerDos === 2) {
+            return `el jugador ${playerUno} escogio papel y el jugador ${playerDos} escogio tijera. GANADOR: ${playerDos}`;
+        }
+        else if (playerDos === 3) {
+            return `el jugador ${playerUno} escogio papel y el jugador ${playerDos} escogio papel. GANADOR: EMPATE!!`;
+        }
+        else {
+            return `el jugador ${playerUno} escogio papel y el jugador ${playerDos} escogio papel. GANADOR: ${playerUno}`;
+        }
+    }
+    else {
+        return `no se escogio una opcion optima`;
+    }
 }
 console.log(janken(cpu, pedroPascal));
