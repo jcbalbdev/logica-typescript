d22fd22922864a31cde5037981729667059207b1
jcbalbdev <jcbalbdev@gmail.com>
2025-04-19 14:38:31 -0500

    Actualización automática: 2025-04-19 14:38:31

--- lv7-juego-piedra-papel-o-tijera.js
+++ lv7-juego-piedra-papel-o-tijera.js
@@ -1,1 +1,5 @@
 "use strict";
+/* Simula un juego entre jugador y CPU (puedes usar Math.random() para CPU). Evalúa el ganador con condiciones. */
+/* piedra = 1 , tijera = 2 , tijera = 3*/
+let jugador = 1;
+let cpu = Math.floor(Math.random() * 3) + 1;
--- lv7-juego-piedra-papel-o-tijera.ts
+++ lv7-juego-piedra-papel-o-tijera.ts
@@ -1,1 +1,4 @@
-
+/* Simula un juego entre jugador y CPU (puedes usar Math.random() para CPU). Evalúa el ganador con condiciones. */
+/* piedra = 1 , tijera = 2 , tijera = 3*/
+let jugador: number = 1;
+let cpu: number = Math.floor(Math.random()*3) + 1;
