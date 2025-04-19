39623056768005015f463f0265a4a45f092a0ab6
jcbalbdev <jcbalbdev@gmail.com>
2025-04-19 14:51:35 -0500

    Actualización automática: 2025-04-19 14:51:34

--- lv7-juego-piedra-papel-o-tijera.js
+++ lv7-juego-piedra-papel-o-tijera.js
@@ -1,5 +1,43 @@
 "use strict";
 /* Simula un juego entre jugador y CPU (puedes usar Math.random() para CPU). Evalúa el ganador con condiciones. */
-/* piedra = 1 , tijera = 2 , tijera = 3*/
-let jugador = 1;
+/* piedra = 1 , tijera = 2 , papel = 3*/
+let pedroPascal = 1;
 let cpu = Math.floor(Math.random() * 3) + 1;
+function janken(playerUno, playerDos) {
+    if (playerUno === 1) {
+        if (playerDos == 2) {
+            return `el jugador ${playerUno} escogio piedra y el jugador ${playerDos} escogio tijera. GANADOR: ${playerUno}`;
+        }
+        else if (playerDos == 3) {
+            return `el jugador ${playerUno} escogio piedra y el jugador ${playerDos} escogio papel. GANADOR: ${playerDos}`;
+        }
+        else {
+            return `el jugador ${playerUno} escogio piedra y el jugador ${playerDos} escogio piedra. GANADOR: EMPATE!!`;
+        }
+    }
+    else if (playerUno === 2) {
+        if (playerDos == 2) {
+            return `el jugador ${playerUno} escogio tijera y el jugador ${playerDos} escogio tijera. GANADOR: EMPATE!!`;
+        }
+        else if (playerDos == 3) {
+            return `el jugador ${playerUno} escogio tijera y el jugador ${playerDos} escogio papel. GANADOR: ${playerUno}`;
+        }
+        else {
+            return `el jugador ${playerUno} escogio tijera y el jugador ${playerDos} escogio papel. GANADOR: ${playerDos}`;
+        }
+    }
+    else if (playerUno === 3) {
+        if (playerDos == 2) {
+            return `el jugador ${playerUno} escogio papel y el jugador ${playerDos} escogio tijera. GANADOR: ${playerDos}`;
+        }
+        else if (playerDos == 3) {
+            return `el jugador ${playerUno} escogio papel y el jugador ${playerDos} escogio papel. GANADOR: EMPATE!!`;
+        }
+        else {
+            return `el jugador ${playerUno} escogio papel y el jugador ${playerDos} escogio papel. GANADOR: ${playerUno}`;
+        }
+    }
+    else {
+        return `no se escogio una opcion optima`;
+    }
+}
--- lv7-juego-piedra-papel-o-tijera.ts
+++ lv7-juego-piedra-papel-o-tijera.ts
@@ -1,4 +1,34 @@
 /* Simula un juego entre jugador y CPU (puedes usar Math.random() para CPU). Evalúa el ganador con condiciones. */
-/* piedra = 1 , tijera = 2 , tijera = 3*/
-let jugador: number = 1;
+/* piedra = 1 , tijera = 2 , papel = 3*/
+let pedroPascal: number = 1;
 let cpu: number = Math.floor(Math.random()*3) + 1;
+
+function janken(playerUno: number,playerDos: number): string{
+  if(playerUno === 1){
+    if(playerDos == 2){
+      return `el jugador ${playerUno} escogio piedra y el jugador ${playerDos} escogio tijera. GANADOR: ${playerUno}`
+    }else if(playerDos == 3){
+      return `el jugador ${playerUno} escogio piedra y el jugador ${playerDos} escogio papel. GANADOR: ${playerDos}`
+    }else{
+      return `el jugador ${playerUno} escogio piedra y el jugador ${playerDos} escogio piedra. GANADOR: EMPATE!!`
+    }
+  }else if(playerUno === 2){
+    if(playerDos == 2){
+      return `el jugador ${playerUno} escogio tijera y el jugador ${playerDos} escogio tijera. GANADOR: EMPATE!!`
+    }else if(playerDos == 3){
+      return `el jugador ${playerUno} escogio tijera y el jugador ${playerDos} escogio papel. GANADOR: ${playerUno}`
+    }else{
+      return `el jugador ${playerUno} escogio tijera y el jugador ${playerDos} escogio papel. GANADOR: ${playerDos}`
+    }
+  }else if(playerUno === 3){
+    if(playerDos == 2){
+      return `el jugador ${playerUno} escogio papel y el jugador ${playerDos} escogio tijera. GANADOR: ${playerDos}`
+    }else if(playerDos == 3){
+      return `el jugador ${playerUno} escogio papel y el jugador ${playerDos} escogio papel. GANADOR: EMPATE!!`
+    }else{
+      return `el jugador ${playerUno} escogio papel y el jugador ${playerDos} escogio papel. GANADOR: ${playerUno}`
+    }
+  }else{
+    return `no se escogio una opcion optima`;
+  }
+} 
