2da6d88b1ba3bd1342b985da356d77abe4059316
jcbalbdev <jcbalbdev@gmail.com>
2025-04-18 20:01:11 -0500

    Actualización automática: 2025-04-18 20:01:11

--- lv5-comparacion-de-contraseñas.js
+++ lv5-comparacion-de-contraseñas.js
@@ -1,1 +1,18 @@
 "use strict";
+/* Simula la validación de una contraseña ingresada dos veces. Muestra si coinciden o no y si cumple longitud mínima. */
+let primerIngreso = "dominatrix12345";
+let segundoIngreso = "dominatrix12345";
+function validacionPass(primerIngreso, segundoIngreso, min) {
+    let longitudPass = primerIngreso.length;
+    if (primerIngreso === segundoIngreso) {
+        if (longitudPass >= 8) {
+            return `la contraseña es valida`;
+        }
+        else {
+            return `la contraseña no es valida, numero insuficiente de caracteres`;
+        }
+    }
+    else {
+        return `las contraseñas no coinciden`;
+    }
+}
--- lv5-comparacion-de-contraseñas.ts
+++ lv5-comparacion-de-contraseñas.ts
@@ -1,1 +1,17 @@
+/* Simula la validación de una contraseña ingresada dos veces. Muestra si coinciden o no y si cumple longitud mínima. */
 
+let primerIngreso: string = "dominatrix12345";
+let segundoIngreso: string = "dominatrix12345"; 
+
+function validacionPass(primerIngreso: string,segundoIngreso: string,min: number): string{
+  let longitudPass: number = primerIngreso.length;
+  if(primerIngreso === segundoIngreso){
+    if(longitudPass >= 8){
+      return `la contraseña es valida`;
+    }else{
+      return `la contraseña no es valida, numero insuficiente de caracteres`;
+    }
+  }else{
+    return `las contraseñas no coinciden`;
+  }
+}
