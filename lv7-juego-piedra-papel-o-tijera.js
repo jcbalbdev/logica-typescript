1de29e3a65e5b05a3dbe177da66493089726d580
jcbalbdev <jcbalbdev@gmail.com>
2025-04-19 16:20:08 -0500

    Actualización automática: 2025-04-19 16:20:08

--- lv7-juego-piedra-papel-o-tijera.js
+++ lv7-juego-piedra-papel-o-tijera.js
@@ -1,14 +1,30 @@
 "use strict";
 /* Simula un juegode janken(piedra,papel o tijera) entre jugador y CPU (puedes usar Math.random() para CPU). Evalúa el ganador con condiciones. */
 /* piedra = 1 , tijera = 2 , papel = 3*/
-var Opciones;
-(function (Opciones) {
-    Opciones[Opciones["Piedra"] = 0] = "Piedra";
-    Opciones[Opciones["Papel"] = 1] = "Papel";
-    Opciones[Opciones["Tijera"] = 2] = "Tijera";
-})(Opciones || (Opciones = {}));
+var Opcion;
+(function (Opcion) {
+    Opcion[Opcion["Piedra"] = 0] = "Piedra";
+    Opcion[Opcion["Papel"] = 1] = "Papel";
+    Opcion[Opcion["Tijera"] = 2] = "Tijera";
+})(Opcion || (Opcion = {}));
 let nombres = {
-    [Opciones.Piedra]: "piedra",
-    [Opciones.Papel]: "papel",
-    [Opciones.Tijera]: "tijera"
+    [Opcion.Piedra]: "piedra",
+    [Opcion.Papel]: "papel",
+    [Opcion.Tijera]: "tijera"
 };
+function janken(player, cpu) {
+    let OpcionPlayer = nombres[player];
+    let OpcionCpu = nombres[cpu];
+    if (player === cpu) {
+        return `Ambos eligieron ${OpcionPlayer}. Resultado: ¡EMPATE!`;
+    }
+    let ganoPlayer = (player === Opcion.Piedra && cpu === Opcion.Tijera) ||
+        (player === Opcion.Tijera && cpu === Opcion.Papel) ||
+        (player === Opcion.Papel && cpu === Opcion.Piedra);
+    let ganador = ganoPlayer ? "Jugador 1" : "CPU";
+    let eleccionDelGanador = ganoPlayer ? OpcionPlayer : OpcionCpu;
+    return `Jugador 1 eligió ${OpcionPlayer} y CPU eligió ${OpcionCpu}. GANADOR: ${ganador} con ${eleccionDelGanador}`;
+}
+let player = Opcion.Piedra;
+let cpu = Math.floor(Math.random() * 3) + 1;
+console.log(janken(player, cpu));
--- lv7-juego-piedra-papel-o-tijera.ts
+++ lv7-juego-piedra-papel-o-tijera.ts
@@ -1,17 +1,39 @@
 /* Simula un juegode janken(piedra,papel o tijera) entre jugador y CPU (puedes usar Math.random() para CPU). Evalúa el ganador con condiciones. */
 /* piedra = 1 , tijera = 2 , papel = 3*/
 
-enum Opciones{
+enum Opcion{
   Piedra = 0,
   Papel = 1,
   Tijera = 2
 }
 
-let nombres : Record<Opciones,string> = {
-  [Opciones.Piedra] : "piedra",
-  [Opciones.Papel] : "papel",
-  [Opciones.Tijera] : "tijera"
+let nombres : Record<Opcion,string> = {
+  [Opcion.Piedra] : "piedra",
+  [Opcion.Papel] : "papel",
+  [Opcion.Tijera] : "tijera"
 }
 
+function janken(player: Opcion,cpu: Opcion): string{
+  let OpcionPlayer: string = nombres[player];
+  let OpcionCpu: string = nombres[cpu] ;
+  if(player === cpu){
+    return `Ambos eligieron ${OpcionPlayer}. Resultado: ¡EMPATE!`;
+  }
+
+  let ganoPlayer = (player === Opcion.Piedra && cpu === Opcion.Tijera) ||
+  (player === Opcion.Tijera && cpu === Opcion.Papel) ||
+  (player === Opcion.Papel && cpu === Opcion.Piedra);
+
+  let ganador = ganoPlayer?"Jugador 1" : "CPU";
+  let eleccionDelGanador = ganoPlayer ? OpcionPlayer : OpcionCpu;
+
+  return `Jugador 1 eligió ${OpcionPlayer} y CPU eligió ${OpcionCpu}. GANADOR: ${ganador} con ${eleccionDelGanador}`;
+
+}
+
+let player: Opcion = Opcion.Piedra;
+let cpu: Opcion = Math.floor(Math.random()*3) + 1;
+
+console.log(janken(player,cpu));
 
 
