e15af7a568c975175e159cc8df5b870f0c1743b1
jcbalbdev <jcbalbdev@gmail.com>
2025-04-19 16:37:16 -0500

    Actualización automática: 2025-04-19 16:37:16

--- lv8-reto-logico-con-condiciones.js
+++ lv8-reto-logico-con-condiciones.js
@@ -1,1 +1,14 @@
 "use strict";
+/* Evalúa tres números enteros. Si alguno es igual a la suma de los otros dos, devuelve true, si no, false. */
+let np = 2;
+let ns = 3;
+let nt = 4;
+function comparando(pv, sv, tv) {
+    if ((pv - (sv + tv) === 0) || (sv - (pv + tv) === 0) || (tv - (sv + pv) === 0)) {
+        return true;
+    }
+    else {
+        return false;
+    }
+}
+console.log(comparando(np, ns, nt));
--- lv8-reto-logico-con-condiciones.ts
+++ lv8-reto-logico-con-condiciones.ts
@@ -1,1 +1,15 @@
+/* Evalúa tres números enteros. Si alguno es igual a la suma de los otros dos, devuelve true, si no, false. */
 
+let np: number = 2;
+let ns: number = 3;
+let nt: number = 4;
+
+function comparando(pv: number,sv: number,tv: number): boolean{
+  if ((pv-(sv+tv) === 0) || (sv-(pv+tv) === 0) || (tv-(sv+pv) === 0) ) {
+    return true;
+  } else {
+    return false
+  }
+}
+
+console.log(comparando(np,ns,nt));
