fed21d5f015e8ee1f961f453d7a18a797a2c73ff
jcbalbdev <jcbalbdev@gmail.com>
2025-04-19 14:57:45 -0500

    Actualización automática: 2025-04-19 14:57:45

--- lv7-juego-piedra-papel-o-tijera.js
+++ lv7-juego-piedra-papel-o-tijera.js
@@ -4,41 +4,6 @@
 let pedroPascal = 1;
 let cpu = Math.floor(Math.random() * 3) + 1;
 function janken(playerUno, playerDos) {
-    if (playerUno === 1) {
-        if (playerDos === 2) {
-            return `el jugador ${playerUno} escogio piedra y el jugador ${playerDos} escogio tijera. GANADOR: ${playerUno}`;
-        }
-        else if (playerDos === 3) {
-            return `el jugador ${playerUno} escogio piedra y el jugador ${playerDos} escogio papel. GANADOR: ${playerDos}`;
-        }
-        else {
-            return `el jugador ${playerUno} escogio piedra y el jugador ${playerDos} escogio piedra. GANADOR: EMPATE!!`;
-        }
-    }
-    else if (playerUno === 2) {
-        if (playerDos === 2) {
-            return `el jugador ${playerUno} escogio tijera y el jugador ${playerDos} escogio tijera. GANADOR: EMPATE!!`;
-        }
-        else if (playerDos === 3) {
-            return `el jugador ${playerUno} escogio tijera y el jugador ${playerDos} escogio papel. GANADOR: ${playerUno}`;
-        }
-        else {
-            return `el jugador ${playerUno} escogio tijera y el jugador ${playerDos} escogio papel. GANADOR: ${playerDos}`;
-        }
-    }
-    else if (playerUno === 3) {
-        if (playerDos === 2) {
-            return `el jugador ${playerUno} escogio papel y el jugador ${playerDos} escogio tijera. GANADOR: ${playerDos}`;
-        }
-        else if (playerDos === 3) {
-            return `el jugador ${playerUno} escogio papel y el jugador ${playerDos} escogio papel. GANADOR: EMPATE!!`;
-        }
-        else {
-            return `el jugador ${playerUno} escogio papel y el jugador ${playerDos} escogio papel. GANADOR: ${playerUno}`;
-        }
-    }
-    else {
-        return `no se escogio una opcion optima`;
-    }
+    return `${playerUno} y ${playerDos}`;
 }
 console.log(janken(cpu, pedroPascal));
--- lv7-juego-piedra-papel-o-tijera.ts
+++ lv7-juego-piedra-papel-o-tijera.ts
@@ -4,33 +4,7 @@ let pedroPascal: number = 1;
 let cpu: number = Math.floor(Math.random()*3) + 1;
 
 function janken(playerUno: number,playerDos: number): string{
-  if(playerUno === 1){
-    if(playerDos === 2){
-      return `el jugador ${playerUno} escogio piedra y el jugador ${playerDos} escogio tijera. GANADOR: ${playerUno}`
-    }else if(playerDos === 3){
-      return `el jugador ${playerUno} escogio piedra y el jugador ${playerDos} escogio papel. GANADOR: ${playerDos}`
-    }else{
-      return `el jugador ${playerUno} escogio piedra y el jugador ${playerDos} escogio piedra. GANADOR: EMPATE!!`
-    }
-  }else if(playerUno === 2){
-    if(playerDos === 2){
-      return `el jugador ${playerUno} escogio tijera y el jugador ${playerDos} escogio tijera. GANADOR: EMPATE!!`
-    }else if(playerDos === 3){
-      return `el jugador ${playerUno} escogio tijera y el jugador ${playerDos} escogio papel. GANADOR: ${playerUno}`
-    }else{
-      return `el jugador ${playerUno} escogio tijera y el jugador ${playerDos} escogio papel. GANADOR: ${playerDos}`
-    }
-  }else if(playerUno === 3){
-    if(playerDos === 2){
-      return `el jugador ${playerUno} escogio papel y el jugador ${playerDos} escogio tijera. GANADOR: ${playerDos}`
-    }else if(playerDos === 3){
-      return `el jugador ${playerUno} escogio papel y el jugador ${playerDos} escogio papel. GANADOR: EMPATE!!`
-    }else{
-      return `el jugador ${playerUno} escogio papel y el jugador ${playerDos} escogio papel. GANADOR: ${playerUno}`
-    }
-  }else{
-    return `no se escogio una opcion optima`;
-  }
+  return `${playerUno} y ${playerDos}`
 } 
 
 console.log(janken(cpu,pedroPascal));
