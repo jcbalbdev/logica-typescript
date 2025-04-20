e03f0dac822c249b2c818675f70166e5e83865d5
jcbalbdev <jcbalbdev@gmail.com>
2025-04-19 19:41:20 -0500

    Actualización automática: 2025-04-19 19:41:20

--- lv6-comparacion-de-fechas.ts
+++ lv6-comparacion-de-fechas.ts
@@ -2,3 +2,10 @@
 
 let fechaUno: Date = new Date("2025-04-01");
 let fechaDos: Date = new Date("2025-04-03");
+
+
+function validarFechas(fechaUno: Date,fechaDos: Date): string{
+  return ((fechaDos.getTime - fechaUno.getTime)>0)?`ayer fue ${fechaUno}`:`ayer fue ${fechaDos}`;
+}
+
+console.log(validarFechas(fechaUno,fechaDos));
