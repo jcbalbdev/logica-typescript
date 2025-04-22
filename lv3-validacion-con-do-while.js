5d5b9b0c8a82063c79da46b171af0a52108badc7
jcbalbdev <jcbalbdev@gmail.com>
2025-04-22 11:37:01 -0500

    Actualización automática: 2025-04-22 11:37:01

--- lv3-validacion-con-do-while.js
+++ lv3-validacion-con-do-while.js
@@ -1,7 +1,9 @@
 "use strict";
 /* Simula una entrada de usuario que se repite hasta que ingrese una contraseña correcta. */
 let contraseña = "";
+let passCorrecta = "pass";
 do {
+    console.log("Verificando contraseña:", contraseña);
     contraseña = "pass";
-} while (contraseña !== "pass");
-console.log("contraseña correcta");
+} while (contraseña !== passCorrecta);
+console.log("Contraseña correcta. Acceso permitido.");
--- lv3-validacion-con-do-while.ts
+++ lv3-validacion-con-do-while.ts
@@ -1,10 +1,12 @@
 /* Simula una entrada de usuario que se repite hasta que ingrese una contraseña correcta. */
 
 let contraseña: string = "";
+let passCorrecta: string = "pass"
 
 do {
+  console.log("Verificando contraseña:", contraseña);
   contraseña = "pass";
   
-} while (contraseña!=="pass");
+} while (contraseña!==passCorrecta);
 
-console.log("contraseña correcta")
+console.log("Contraseña correcta. Acceso permitido.");
