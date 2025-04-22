48b110d4c79b48d0ca4e9e98044420a82cda004e
jcbalbdev <jcbalbdev@gmail.com>
2025-04-22 17:45:43 -0500

    Actualización automática: 2025-04-22 17:45:43

--- lv5-numero-primo.js
+++ lv5-numero-primo.js
@@ -1,16 +1,16 @@
 "use strict";
 /* Dado un número, determina si es primo (tiene solo dos divisores: 1 y él mismo). */
-let esPrimo = 11;
+let esPrimo = 37;
 function numeroPrimo(numero) {
     let contador = 0;
-    for (let i = 1; i <= numero; i++) {
+    for (let i = 1; i <= Math.sqrt(numero); i++) {
         if (numero % i === 0) {
             contador++;
         }
     }
     if (contador > 2) {
-        return `no es primo`;
+        return `${numero} no es primo`;
     }
-    return `es primo`;
+    return `${numero} es primo`;
 }
 console.log(numeroPrimo(esPrimo));
