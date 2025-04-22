594dbc9435027c687a63919cfa3f05fa2d90163b
jcbalbdev <jcbalbdev@gmail.com>
2025-04-22 11:59:20 -0500

    Actualización automática: 2025-04-22 11:59:20

--- lv3-patron-de-asteriscos-triangulo-recto.js
+++ lv3-patron-de-asteriscos-triangulo-recto.js
@@ -7,3 +7,8 @@ Muestra en consola un triangulo hecho con asteriscos:
 ****
 *****
  */
+let linea = "";
+for (let i = 0; i < 5; i++) {
+    linea += "*";
+    console.log(linea);
+}
--- lv3-patron-de-asteriscos-triangulo-recto.ts
+++ lv3-patron-de-asteriscos-triangulo-recto.ts
@@ -6,3 +6,10 @@ Muestra en consola un triangulo hecho con asteriscos:
 ****
 *****
  */
+
+let linea: string = "";
+
+for(let i: number = 0 ;i<5;i++){
+  linea+="*";
+  console.log(linea);
+}
