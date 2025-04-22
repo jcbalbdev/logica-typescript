e52dbd1ca053a0f6cbb0dcd435fff8271e83e650
jcbalbdev <jcbalbdev@gmail.com>
2025-04-22 11:09:37 -0500

    Actualización automática: 2025-04-22 11:09:37

--- lv2-numeros-pares-del-1-al-50.js
+++ lv2-numeros-pares-del-1-al-50.js
@@ -1,7 +1,5 @@
 "use strict";
 /* Imprime los números pares entre 1 y 50 usando un bucle for. */
-for (let i = 1; i <= 50; i++) {
-    if (i % 2 === 0) {
-        console.log(i);
-    }
+for (let i = 2; i <= 50; i += 2) {
+    console.log(i);
 }
--- lv2-numeros-pares-del-1-al-50.ts
+++ lv2-numeros-pares-del-1-al-50.ts
@@ -1,7 +1,7 @@
 /* Imprime los números pares entre 1 y 50 usando un bucle for. */
 
-for(let i: number = 1;i<=50;i++){
-  if(i%2===0){
-    console.log(i);
-  }
+for(let i: number = 2;i<=50;i+=2){
+  console.log(i);
 }
+
+
