d209e6d94044c943fd45b4a3af100e6fc7ed1bab
jcbalbdev <jcbalbdev@gmail.com>
2025-04-22 17:32:31 -0500

    Actualización automática: 2025-04-22 17:32:31

--- lv5-numero-primo.js
+++ lv5-numero-primo.js
@@ -1,5 +1,3 @@
 "use strict";
 /* Dado un número, determina si es primo (tiene solo dos divisores: 1 y él mismo). */
 let esPrimo = 4;
-function numeroPrimo(numero) {
-}
--- lv5-numero-primo.ts
+++ lv5-numero-primo.ts
@@ -1,6 +1,4 @@
 /* Dado un número, determina si es primo (tiene solo dos divisores: 1 y él mismo). */
 let esPrimo: number  = 4;
 
-function numeroPrimo(numero: number): string{
 
-}
