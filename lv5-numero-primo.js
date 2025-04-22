d427535d433088696d7001d0f3e6cdf007f5cdd4
jcbalbdev <jcbalbdev@gmail.com>
2025-04-22 17:37:34 -0500

    Actualización automática: 2025-04-22 17:37:34

--- lv5-numero-primo.js
+++ lv5-numero-primo.js
@@ -1,3 +1,15 @@
 "use strict";
 /* Dado un número, determina si es primo (tiene solo dos divisores: 1 y él mismo). */
 let esPrimo = 4;
+function numeroPrimo(numero) {
+    let contador = 0;
+    for (let i = 1; i <= numero; i++) {
+        if (numero % i === 0) {
+            contador++;
+        }
+    }
+    if (contador !== 2) {
+        return `no es primo`;
+    }
+    return `es primo`;
+}
--- lv5-numero-primo.ts
+++ lv5-numero-primo.ts
@@ -1,4 +1,17 @@
 /* Dado un número, determina si es primo (tiene solo dos divisores: 1 y él mismo). */
 let esPrimo: number  = 4;
 
+function numeroPrimo(numero: number): string{
+  let contador: number = 0;
+  for(let i=1;i<=numero;i++){
+    if(numero%i===0){
+      contador++;
+    }
+  }
 
+  if(contador!==2){
+    return `no es primo`
+  }
+
+  return `es primo`
+}
