9bf11a0324de4be504bae405d63c544c98a1da38
jcbalbdev <jcbalbdev@gmail.com>
2025-04-22 16:52:40 -0500

    Actualización automática: 2025-04-22 16:52:40

--- lv4-potencia-con-bucle.js
+++ lv4-potencia-con-bucle.js
@@ -1,12 +1,25 @@
 "use strict";
 /* Calcula base^exponente sin usar Math.pow(), solo con bucles. */
-let numX = -3;
-let exponente = 3;
+let numX = -2;
+let exponente = -2;
 function potencia(numero, exponente) {
     let operacion = 1;
     for (let i = 0; i < exponente; i++) {
         operacion *= numero;
     }
-    return `${numero} elevado a ${exponente} es ${operacion}`;
+    return operacion;
 }
-console.log(potencia(numX, exponente));
+function potenciaGeneral(numero, exponente) {
+    let respuesta;
+    if (exponente < 0) {
+        respuesta = 1 / potencia(numero, -exponente);
+    }
+    else if (exponente > 0) {
+        respuesta = potencia(numero, exponente);
+    }
+    else {
+        respuesta = 0;
+    }
+    return `${numero} elevado a ${exponente} es ${respuesta}`;
+}
+console.log(potenciaGeneral(numX, exponente));
--- lv4-potencia-con-bucle.ts
+++ lv4-potencia-con-bucle.ts
@@ -1,14 +1,26 @@
 /* Calcula base^exponente sin usar Math.pow(), solo con bucles. */
 
-let numX: number = -3;
-let exponente: number = 3;
+let numX: number = -2;
+let exponente: number = -2;
 
-function potencia(numero: number,exponente: number): string{
+function potencia(numero: number,exponente: number): number{
   let operacion: number = 1;
   for(let i: number =0;i<exponente;i++){
     operacion*= numero;
   }
-  return `${numero} elevado a ${exponente} es ${operacion}`;
+  return operacion;
 }
 
-console.log(potencia(numX,exponente));
+function potenciaGeneral(numero: number,exponente: number): string{
+  let respuesta: number;
+  if(exponente<0){
+    respuesta = 1/potencia(numero,-exponente);
+  }else if(exponente>0){
+    respuesta = potencia(numero,exponente);
+  }else{
+    respuesta = 0;
+  }
+  return `${numero} elevado a ${exponente} es ${respuesta}`;
+}
+
+console.log(potenciaGeneral(numX,exponente));
