6c26a32b8b526cbd79513e177603274f1f189662
jcbalbdev <jcbalbdev@gmail.com>
2025-04-22 11:00:30 -0500

    Actualización automática: 2025-04-22 11:00:30

--- lv2-suma-de-los-primeros-100-numeros.js
+++ lv2-suma-de-los-primeros-100-numeros.js
@@ -1,1 +1,7 @@
 "use strict";
+/* Usa un bucle para sumar los números del 1 al 100 y muestra el resultado final. */
+let sumaCien = 0;
+for (let i = 1; i <= 100; i++) {
+    sumaCien += i;
+}
+console.log(sumaCien);
