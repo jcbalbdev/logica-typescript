3c465e4bbea42d678a8dcc6a56d1de5f29f3ba30
jcbalbdev <jcbalbdev@gmail.com>
2025-04-22 11:47:06 -0500

    Actualización automática: 2025-04-22 11:47:06

--- lv3-suma-de-numeros-hasta-que-sea-mayor-a-100.js
+++ lv3-suma-de-numeros-hasta-que-sea-mayor-a-100.js
@@ -1,1 +1,9 @@
 "use strict";
+/* Genera números aleatorios del 1 al 10 y súmalos hasta superar 100. Usa while. */
+let aleatorio;
+let sumaAleatorio = 0;
+while (sumaAleatorio < 100) {
+    aleatorio = Math.floor(Math.random() * 100 + 1);
+    console.log(aleatorio);
+    sumaAleatorio += aleatorio;
+}
--- lv3-suma-de-numeros-hasta-que-sea-mayor-a-100.ts
+++ lv3-suma-de-numeros-hasta-que-sea-mayor-a-100.ts
@@ -1,1 +1,9 @@
+/* Genera números aleatorios del 1 al 10 y súmalos hasta superar 100. Usa while. */
+let aleatorio: number ;
+let sumaAleatorio: number = 0;
 
+while(sumaAleatorio<100){
+  aleatorio = Math.floor(Math.random()*100 + 1);
+  console.log(aleatorio);
+  sumaAleatorio+=aleatorio;
+}
