814192014db731b5cc05b18f1f3957b2e434ea68
jcbalbdev <jcbalbdev@gmail.com>
2025-04-22 18:26:04 -0500

    Actualización automática: 2025-04-22 18:26:04

--- lv4-contar-digitos-de-un-numero.js
+++ lv4-contar-digitos-de-un-numero.js
@@ -1,4 +1,4 @@
 "use strict";
 /* Dado un número, cuenta cuántos dígitos tiene (sin convertirlo a string). */
 let miNum = 1234;
-console.log(miNum % 1000);
+console.log(miNum % 10);
--- lv4-contar-digitos-de-un-numero.ts
+++ lv4-contar-digitos-de-un-numero.ts
@@ -1,4 +1,4 @@
 /* Dado un número, cuenta cuántos dígitos tiene (sin convertirlo a string). */
 let miNum: number = 1234
 
-console.log(miNum%1000);
+console.log(miNum%10);
