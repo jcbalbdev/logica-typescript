fb3779cd377910e56ed78187fdc344bf1d6860fe
jcbalbdev <jcbalbdev@gmail.com>
2025-04-22 17:55:48 -0500

    Actualización automática: 2025-04-22 17:55:48

--- lv4-factorial.js
+++ lv4-factorial.js
@@ -1,1 +1,16 @@
 "use strict";
+/* Calcula el factorial de un número n. */
+let numPrueba = 5;
+function factorial(num) {
+    let comodin = 1;
+    for (let i = 0; i < num; i++) {
+        comodin = comodin * (i + 1);
+    }
+    return `el factorial de ${num} es ${comodin}`;
+}
+console.log(factorial(numPrueba));
+/*
+4x3x2x1
+n=1
+n=nx(i+1)
+*/ 
--- lv4-factorial.ts
+++ lv4-factorial.ts
@@ -1,1 +1,18 @@
+/* Calcula el factorial de un número n. */
 
+let numPrueba: number = 5;
+
+function factorial(num: number): string{
+  let comodin: number=1;
+  for(let i: number=0;i<num;i++){
+    comodin=comodin*(i+1);
+  }
+  return `el factorial de ${num} es ${comodin}`;
+}
+
+console.log(factorial(numPrueba));
+/* 
+4x3x2x1
+n=1
+n=nx(i+1)
+*/
