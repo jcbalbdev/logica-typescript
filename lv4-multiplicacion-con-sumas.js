62caeae9d7c022847239dcb0ad66a9d8dc26cdd9
jcbalbdev <jcbalbdev@gmail.com>
2025-04-22 12:08:25 -0500

    Actualización automática: 2025-04-22 12:08:25

--- lv4-multiplicacion-con-sumas.js
+++ lv4-multiplicacion-con-sumas.js
@@ -1,1 +1,12 @@
 "use strict";
+/* Realiza una multiplicación de dos números sin usar *, solo sumas repetidas. */
+let nume = 2;
+let otroNume = 3;
+function multiplicación(a, b) {
+    let operacion = 0;
+    for (let i = 0; i < b; i++) {
+        operacion += a;
+    }
+    return `la multiplicacion de ${a} y ${b} es : ${operacion}`;
+}
+console.log(multiplicación(nume, otroNume));
--- lv4-multiplicacion-con-sumas.ts
+++ lv4-multiplicacion-con-sumas.ts
@@ -1,1 +1,15 @@
+/* Realiza una multiplicación de dos números sin usar *, solo sumas repetidas. */
 
+let nume: number = 2;
+let otroNume: number = 3;
+
+function multiplicación(a: number,b :number): string{
+  let operacion: number = 0;
+  for (let i: number = 0; i < b; i++) {
+    operacion+=a
+    
+  }
+  return `la multiplicacion de ${a} y ${b} es : ${operacion}`;
+}
+
+console.log(multiplicación(nume,otroNume));
