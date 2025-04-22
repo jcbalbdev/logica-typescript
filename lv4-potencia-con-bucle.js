d6d9d3554abba8aa56a7c54e4bbff1e4cb26123a
jcbalbdev <jcbalbdev@gmail.com>
2025-04-22 16:44:33 -0500

    Actualización automática: 2025-04-22 16:44:33

--- lv4-potencia-con-bucle.js
+++ lv4-potencia-con-bucle.js
@@ -1,1 +1,12 @@
 "use strict";
+/* Calcula base^exponente sin usar Math.pow(), solo con bucles. */
+let numX = 1;
+let exponente = 4;
+function potencia(numero, exponente) {
+    let operacion = 1;
+    for (let i = 0; i < exponente; i++) {
+        operacion *= numero;
+    }
+    return `${numero} elevado a ${exponente} es ${operacion}`;
+}
+console.log(potencia(numX, exponente));
--- lv4-potencia-con-bucle.ts
+++ lv4-potencia-con-bucle.ts
@@ -1,1 +1,14 @@
+/* Calcula base^exponente sin usar Math.pow(), solo con bucles. */
 
+let numX: number = 1;
+let exponente: number = 4;
+
+function potencia(numero: number,exponente: number): string{
+  let operacion: number = 1;
+  for(let i: number =0;i<exponente;i++){
+    operacion*= numero;
+  }
+  return `${numero} elevado a ${exponente} es ${operacion}`;
+}
+
+console.log(potencia(numX,exponente));
