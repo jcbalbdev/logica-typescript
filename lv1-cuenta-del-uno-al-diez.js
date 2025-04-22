27a7e436183dfb22c88ae70ee9da1d2976984079
jcbalbdev <jcbalbdev@gmail.com>
2025-04-22 10:54:23 -0500

    Actualización automática: 2025-04-22 10:54:22

--- lv1-cuenta-del-uno-al-diez.js
+++ lv1-cuenta-del-uno-al-diez.js
@@ -1,1 +1,5 @@
 "use strict";
+/* Usa un bucle for para imprimir del 1 al 10 en consola. */
+for (let i = 1; i <= 10; i++) {
+    console.log(i);
+}
--- lv1-cuenta-del-uno-al-diez.ts
+++ lv1-cuenta-del-uno-al-diez.ts
@@ -1,1 +1,5 @@
+/* Usa un bucle for para imprimir del 1 al 10 en consola. */
 
+for (let i: number = 1; i <= 10; i++) {
+  console.log(i);
+}
