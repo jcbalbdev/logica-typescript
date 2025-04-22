0e59c28b8142f9933ab6de6fd5c4a348d90803bc
jcbalbdev <jcbalbdev@gmail.com>
2025-04-22 18:47:18 -0500

    Actualización automática: 2025-04-22 18:47:18

--- lv4-contar-digitos-de-un-numero.js
+++ lv4-contar-digitos-de-un-numero.js
@@ -1,17 +1,18 @@
 "use strict";
 /* Dado un número, cuenta cuántos dígitos tiene (sin convertirlo a string). */
-let miNum = 1000;
+let miNum = 3141592;
 function numeroDeDigitos(numero) {
     let cont = 1;
     let residuo;
     let cociente;
-    for (let i = 0; numero >= 10; i++) {
-        residuo = numero % 10;
-        cociente = Math.floor(numero / 10);
+    let newNum = numero;
+    for (let i = 0; newNum >= 10; i++) {
+        residuo = newNum % 10;
+        cociente = Math.floor(newNum / 10);
         cont++;
-        numero = cociente;
+        newNum = cociente;
     }
-    return `el numero tiene ${cont} digitos`;
+    return `el numero ${numero} tiene ${cont} digitos`;
 }
 console.log(numeroDeDigitos(miNum));
 /*
--- lv4-contar-digitos-de-un-numero.ts
+++ lv4-contar-digitos-de-un-numero.ts
@@ -1,19 +1,20 @@
 /* Dado un número, cuenta cuántos dígitos tiene (sin convertirlo a string). */
-let miNum: number = 1000
+let miNum: number = 3141592
 
 function numeroDeDigitos(numero: number): string{
   let cont: number = 1;
   let residuo: number;
   let cociente: number;
+  let newNum: number = numero;
 
-  for (let i = 0; numero>=10 ; i++) {
-    residuo = numero%10;
-    cociente = Math.floor(numero/10);
+  for (let i = 0; newNum>=10 ; i++) {
+    residuo = newNum%10;
+    cociente = Math.floor(newNum/10);
     cont++
-    numero=cociente;
+    newNum=cociente;
   }
   
-  return `el numero tiene ${cont} digitos`
+  return `el numero ${numero} tiene ${cont} digitos`
 }
 
 console.log(numeroDeDigitos(miNum))
