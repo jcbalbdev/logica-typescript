879d9bdd54f39420064f8e561a6d08f51609ed37
jcbalbdev <jcbalbdev@gmail.com>
2025-04-22 18:42:10 -0500

    Actualización automática: 2025-04-22 18:42:10

--- lv4-contar-digitos-de-un-numero.js
+++ lv4-contar-digitos-de-un-numero.js
@@ -1,5 +1,36 @@
 "use strict";
 /* Dado un número, cuenta cuántos dígitos tiene (sin convertirlo a string). */
 let miNum = 1234;
+function numeroDeDigitos(numero) {
+    let cont = 0;
+    let residuo;
+    let cociente;
+    for (let i = 0; numero >= 10; i++) {
+        residuo = numero % 10;
+        cociente = Math.floor(numero / 10);
+        cont++;
+        numero = cociente;
+    }
+}
 console.log(miNum % 10);
 console.log(Math.floor(miNum / 10));
+/*
+n = 1234
+10
+r=4
+q=123
+c=1
+
+n=q
+r=3
+q=12
+
+n=q
+r=2
+q=1
+
+n=1(n<10)
+
+
+
+*/ 
