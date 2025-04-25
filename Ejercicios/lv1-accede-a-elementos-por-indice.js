0233df73f92488dcafc01245940addd47f818ae0
jcbalbdev <jcbalbdev@gmail.com>
2025-04-25 18:45:17 -0500

    Actualización automática: 2025-04-25 18:45:17

--- Ejercicios/lv1-accede-a-elementos-por-indice.js
+++ Ejercicios/lv1-accede-a-elementos-por-indice.js
@@ -1,1 +1,11 @@
 "use strict";
+/* Declara un array de números y muestra el primero, último y la longitud del array. */
+var Cumpleaño;
+(function (Cumpleaño) {
+    Cumpleaño[Cumpleaño["Bruce"] = 1] = "Bruce";
+    Cumpleaño[Cumpleaño["Diana"] = 2] = "Diana";
+    Cumpleaño[Cumpleaño["Clark"] = 12] = "Clark";
+    Cumpleaño[Cumpleaño["Barry"] = 23] = "Barry";
+})(Cumpleaño || (Cumpleaño = {}));
+let cumpleañosMarzo = [Cumpleaño.Bruce, Cumpleaño.Diana, Cumpleaño.Clark, Cumpleaño.Barry];
+console.log(`la cantidad de personas que cumples años en marzo son ${cumpleañosMarzo.length} , el primero en cumplir años es ${cumpleañosMarzo[0]} y el ultimo es ${cumpleañosMarzo[3]}`);
--- Ejercicios/lv1-accede-a-elementos-por-indice.ts
+++ Ejercicios/lv1-accede-a-elementos-por-indice.ts
@@ -1,1 +1,11 @@
+/* Declara un array de números y muestra el primero, último y la longitud del array. */
+enum Cumpleaño{
+  Bruce = 1,
+  Diana  = 2,
+  Clark = 12,
+  Barry = 23
+}
 
+let cumpleañosMarzo: Cumpleaño[] = [Cumpleaño.Bruce,Cumpleaño.Diana,Cumpleaño.Clark,Cumpleaño.Barry];
+
+console.log(`la cantidad de personas que cumples años en marzo son ${cumpleañosMarzo.length} , el primero en cumplir años es ${cumpleañosMarzo[0]} y el ultimo es ${cumpleañosMarzo[3]}`);
