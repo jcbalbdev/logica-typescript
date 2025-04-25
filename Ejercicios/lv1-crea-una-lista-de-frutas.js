05503e04b2389dd31e025b3316ce7c8a9c058edf
jcbalbdev <jcbalbdev@gmail.com>
2025-04-25 18:24:05 -0500

    Actualización automática: 2025-04-25 18:24:05

--- Ejercicios/lv1-crea-una-lista-de-frutas.js
+++ Ejercicios/lv1-crea-una-lista-de-frutas.js
@@ -1,1 +1,11 @@
 "use strict";
+/* Crea un array con frutas y muéstralo con console.log. */
+var Fruta;
+(function (Fruta) {
+    Fruta["Manzana"] = "manzana";
+    Fruta["Platano"] = "platano";
+    Fruta["Mandarina"] = "mandarina";
+    Fruta["Pera"] = "pera";
+})(Fruta || (Fruta = {}));
+let frutas = [Fruta.Manzana, Fruta.Platano, Fruta.Pera, Fruta.Mandarina];
+console.log(`las frutas que necesitamos hoy son ${frutas[0]} ${frutas[1]} ${frutas[2]} ${frutas[3]}`);
--- Ejercicios/lv1-crea-una-lista-de-frutas.ts
+++ Ejercicios/lv1-crea-una-lista-de-frutas.ts
@@ -1,1 +1,11 @@
+/* Crea un array con frutas y muéstralo con console.log. */
+enum Fruta{
+  Manzana = "manzana",
+  Platano = "platano",
+  Mandarina = "mandarina",
+  Pera = "pera"
+}
 
+let frutas: Fruta[] = [Fruta.Manzana,Fruta.Platano,Fruta.Pera,Fruta.Mandarina];
+
+console.log(`las frutas que necesitamos hoy son ${frutas[0]} ${frutas[1]} ${frutas[2]} ${frutas[3]}`);
