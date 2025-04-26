faee759e2b9810d4c43c7a215894201f6dedc2e8
jcbalbdev <jcbalbdev@gmail.com>
2025-04-25 20:07:12 -0500

    Actualización automática: 2025-04-25 20:07:12

--- Ejercicios/lv3-busca-un-elemento-en-el-array.js
+++ Ejercicios/lv3-busca-un-elemento-en-el-array.js
@@ -1,3 +1,11 @@
 "use strict";
 /* Usa includes y indexOf para saber si una heroe está en la lista. */
 let heroes = ["Batman", "Superman", "Flash"];
+function estaEnLaLista(lista, heroe) {
+    if (lista.includes(heroe)) {
+        return `el ${heroe} si esta en la lista`;
+    }
+    else {
+        return `el ${heroe} no esta en la lista`;
+    }
+}
--- Ejercicios/lv3-busca-un-elemento-en-el-array.ts
+++ Ejercicios/lv3-busca-un-elemento-en-el-array.ts
@@ -1,3 +1,11 @@
 /* Usa includes y indexOf para saber si una heroe está en la lista. */
 
 let heroes: string[] = ["Batman", "Superman", "Flash"];
+
+function estaEnLaLista(lista: string[],heroe: string){
+  if(lista.includes(heroe)){
+    return `el ${heroe} si esta en la lista`;
+  }else{
+    return `el ${heroe} no esta en la lista`;
+  }
+}
