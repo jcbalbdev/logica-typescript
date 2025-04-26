4d9768e6efa809424177e89e34f5b26be7a9d0e3
jcbalbdev <jcbalbdev@gmail.com>
2025-04-25 19:54:02 -0500

    Actualización automática: 2025-04-25 19:54:02

--- Ejercicios/lv3-agrega-elimina-y-modifica-elementos.js
+++ Ejercicios/lv3-agrega-elimina-y-modifica-elementos.js
@@ -2,26 +2,31 @@
 /* Usa push, pop, shift, unshift, y reasignación de índice para manipular un array. */
 let misFrutas = ["manzana", "platano", "pera", "naranja"];
 function agregaAlFinal(lista, fruta) {
+    let listaOriginal = [...lista];
     lista.push(fruta);
-    return `se agrego ${fruta} al final de la lista. la nueva lista es ${lista}`;
+    return `se agrego ${fruta} al final de la lista ${listaOriginal}. la nueva lista es ${lista}`;
 }
 console.log(agregaAlFinal(misFrutas, "mandarina"));
 function eliminaElFinal(lista) {
+    let listaOriginal = [...lista];
     lista.pop();
-    return `se elimino la ultima fruta de la lista. la nueva lista es ${lista}`;
+    return `se elimino la ultima fruta de la lista ${listaOriginal}. la nueva lista es ${lista}`;
 }
 console.log(eliminaElFinal(misFrutas));
 function agregaAlInicio(lista, fruta) {
+    let listaOriginal = [...lista];
     lista.unshift(fruta);
-    return `se agrego ${fruta} al principio de la lista. la nueva lista es ${lista}`;
+    return `se agrego ${fruta} al principio de la lista ${listaOriginal}. la nueva lista es ${lista}`;
 }
 console.log(agregaAlInicio(misFrutas, "mango"));
 function eliminaElInicio(lista) {
+    let listaOriginal = [...lista];
     lista.shift();
-    return `se elimino la primera fruta de la lista. la nueva lista es ${lista}`;
+    return `se elimino la primera fruta de la lista ${listaOriginal}. la nueva lista es ${lista}`;
 }
 console.log(eliminaElInicio(misFrutas));
 function cambioDeFruta(lista, posicion, fruta) {
+    let listaOriginal = [...lista];
     lista[posicion] = fruta;
     return `se cambio la fruta de la posicion ${posicion + 1} y se agrego la  fruta ${fruta}. la nueva lista es ${lista} `;
 }
--- Ejercicios/lv3-agrega-elimina-y-modifica-elementos.ts
+++ Ejercicios/lv3-agrega-elimina-y-modifica-elementos.ts
@@ -3,38 +3,43 @@
 let misFrutas: string[] = ["manzana", "platano","pera","naranja"]
 
 function agregaAlFinal(lista: string[],fruta: string): string{
+  let listaOriginal: string[] = [...lista];
   lista.push(fruta);
 
-  return `se agrego ${fruta} al final de la lista. la nueva lista es ${lista}`
+  return `se agrego ${fruta} al final de la lista ${listaOriginal}. la nueva lista es ${lista}`
 }
 
 console.log(agregaAlFinal(misFrutas,"mandarina"))
 
 function eliminaElFinal(lista: string[]){
+  let listaOriginal: string[] = [...lista];
   lista.pop();
 
-  return `se elimino la ultima fruta de la lista. la nueva lista es ${lista}`
+  return `se elimino la ultima fruta de la lista ${listaOriginal}. la nueva lista es ${lista}`
 }
 
 console.log(eliminaElFinal(misFrutas));
 
 function agregaAlInicio(lista: string[],fruta: string): string{
+  let listaOriginal: string[] = [...lista];
   lista.unshift(fruta);
 
-  return `se agrego ${fruta} al principio de la lista. la nueva lista es ${lista}`
+  return `se agrego ${fruta} al principio de la lista ${listaOriginal}. la nueva lista es ${lista}`
 }
 
 console.log(agregaAlInicio(misFrutas,"mango"))
 
 function eliminaElInicio(lista: string[]){
+  let listaOriginal: string[] = [...lista];
   lista.shift();
 
-  return `se elimino la primera fruta de la lista. la nueva lista es ${lista}`
+  return `se elimino la primera fruta de la lista ${listaOriginal}. la nueva lista es ${lista}`
 }
 
 console.log(eliminaElInicio(misFrutas));
 
 function cambioDeFruta(lista: string[],posicion: number,fruta: string){
+  let listaOriginal: string[] = [...lista];
   lista[posicion] = fruta;
 
   return `se cambio la fruta de la posicion ${posicion + 1} y se agrego la  fruta ${fruta}. la nueva lista es ${lista} `
