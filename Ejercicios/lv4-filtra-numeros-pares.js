d5952458a09ab423cf387532c0c3afc00914da75
jcbalbdev <jcbalbdev@gmail.com>
2025-04-25 20:27:36 -0500

    Actualización automática: 2025-04-25 20:27:36

--- Ejercicios/lv4-filtra-numeros-pares.js
+++ Ejercicios/lv4-filtra-numeros-pares.js
@@ -1,5 +1,8 @@
 "use strict";
 /* Usa filter para devolver solo los números pares de un array del 1 al 10. */
-let misPares = [1, 2, 3, 5, 7, 8, 10, 9, 4, 6];
-function buscarPar() {
+let misNumeros = [1, 2, 3, 5, 7, 8, 10, 9, 4, 6];
+function buscarPar(lista) {
+    let listaPares = lista.filter((numero) => numero % 2 === 0);
+    return `la lista de numero es ${lista} y la lista de numeros pares ue contiene esa lista es ${listaPares}`;
 }
+console.log(buscarPar(misNumeros));
--- Ejercicios/lv4-filtra-numeros-pares.ts
+++ Ejercicios/lv4-filtra-numeros-pares.ts
@@ -1,7 +1,11 @@
 /* Usa filter para devolver solo los números pares de un array del 1 al 10. */
 
-let misPares: number[] = [1,2,3,5,7,8,10,9,4,6];
+let misNumeros: number[] = [1,2,3,5,7,8,10,9,4,6];
 
-function buscarPar(){
-  
+function buscarPar(lista: number[]){
+  let listaPares: number[] = lista.filter((numero) => numero%2===0);
+
+  return `la lista de numero es ${lista} y la lista de numeros pares ue contiene esa lista es ${listaPares}`
 }
+
+console.log(buscarPar(misNumeros));
