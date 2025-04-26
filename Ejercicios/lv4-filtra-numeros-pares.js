795e88e3346ae665af96e68e525bfac8f4986076
jcbalbdev <jcbalbdev@gmail.com>
2025-04-25 20:21:31 -0500

    Actualización automática: 2025-04-25 20:21:31

--- Ejercicios/lv4-filtra-numeros-pares.js
+++ Ejercicios/lv4-filtra-numeros-pares.js
@@ -1,1 +1,3 @@
 "use strict";
+/* Usa filter para devolver solo los números pares de un array del 1 al 20. */
+let misPares = [1, 2, 3, 5, 1, 8, 9, 22, 314];
--- Ejercicios/lv4-filtra-numeros-pares.ts
+++ Ejercicios/lv4-filtra-numeros-pares.ts
@@ -1,1 +1,3 @@
+/* Usa filter para devolver solo los números pares de un array del 1 al 20. */
 
+let misPares: number[] = [1,2,3,5,1,8,9,22,314];
