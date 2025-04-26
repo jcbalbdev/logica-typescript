08390e53c41d6112a53eb22180825e814d2b2b56
jcbalbdev <jcbalbdev@gmail.com>
2025-04-25 20:54:53 -0500

    Actualización automática: 2025-04-25 20:54:53

--- Ejercicios/lv4-transforma-elementos-con-map.js
+++ Ejercicios/lv4-transforma-elementos-con-map.js
@@ -2,5 +2,7 @@
 /* Usa map para multiplicar todos los elementos de un array por 2 */
 let misImpares = [1, 3, 5, 7, 9];
 function porDos(lista) {
-    return `la lista de numero es ${lista} y la lista de numeros pares ue contiene esa lista es ${listaPares}`;
+    let listaNueva = lista.map((numero) => numero * 2);
+    return `la lista de numero es ${lista} y si multimplicamos los elementos de la lista por 2 me genera esta lista ${listaNueva}`;
 }
+console.log(porDos(misImpares));
--- Ejercicios/lv4-transforma-elementos-con-map.ts
+++ Ejercicios/lv4-transforma-elementos-con-map.ts
@@ -3,7 +3,8 @@
 let misImpares: number[] = [1,3,5,7,9];
 
 function porDos(lista: number[]): string{
-  
+  let listaNueva: number[] = lista.map((numero) => numero*2);
 
-  return `la lista de numero es ${lista} y la lista de numeros pares ue contiene esa lista es ${listaPares}`;
+  return `la lista de numero es ${lista} y si multimplicamos los elementos de la lista por 2 me genera esta lista ${listaNueva}`;
 }
+console.log(porDos(misImpares));
