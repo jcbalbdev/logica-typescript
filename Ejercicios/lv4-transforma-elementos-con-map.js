35eab7326a2bd658ae49cf7be2d6b2e7bc981c52
jcbalbdev <jcbalbdev@gmail.com>
2025-04-25 20:38:45 -0500

    Actualización automática: 2025-04-25 20:38:45

--- Ejercicios/lv4-transforma-elementos-con-map.js
+++ Ejercicios/lv4-transforma-elementos-con-map.js
@@ -1,2 +1,5 @@
 "use strict";
 /* Usa map para multiplicar todos los elementos de un array por 2 */
+let misImpares = [1, 3, 5, 7, 9];
+function porDos() {
+}
--- Ejercicios/lv4-transforma-elementos-con-map.ts
+++ Ejercicios/lv4-transforma-elementos-con-map.ts
@@ -1,1 +1,7 @@
 /* Usa map para multiplicar todos los elementos de un array por 2 */
+
+let misImpares: number[] = [1,3,5,7,9];
+
+function porDos(): string{
+
+}
