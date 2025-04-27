76b8664a3a4ba45b8f9ed32262ac3346c1b054c3
jcbalbdev <jcbalbdev@gmail.com>
2025-04-27 16:58:26 -0500

    Actualización automática: 2025-04-27 16:58:26

--- Ejercicios/lv5-suma-total-con-reduce.js
+++ Ejercicios/lv5-suma-total-con-reduce.js
@@ -1,1 +1,3 @@
 "use strict";
+/* Usa reduce para sumar todos los elementos de un array numérico. */
+let listaNum = [1, 2, 3, 4, 1, 5, 7, 9, 10];
--- Ejercicios/lv5-suma-total-con-reduce.ts
+++ Ejercicios/lv5-suma-total-con-reduce.ts
@@ -1,1 +1,4 @@
+/* Usa reduce para sumar todos los elementos de un array numérico. */
+let listaNum: number[] = [1,2,3,4,1,5,7,9,10];
+
 
