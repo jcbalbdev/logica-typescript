c555fed57e72e4548b57fe7cb975a1f1190dc2f0
jcbalbdev <jcbalbdev@gmail.com>
2025-04-27 17:33:49 -0500

    Actualización automática: 2025-04-27 17:33:49

--- Ejercicios/lv5-eliminar-duplicados-con-set.js
+++ Ejercicios/lv5-eliminar-duplicados-con-set.js
@@ -1,2 +1,8 @@
 "use strict";
 /* Dado un array con duplicados, devuelve uno nuevo solo con valores únicos. */
+let duplicados = [1, 2, 1, 3, 4, 1, 4, 5, 6, 7, 9, 2, 3, 4, 10];
+function arraySinDuplicados(lista) {
+    let valoresUnicos = Array.from(new Set(lista));
+    return `la lista inicial es : ${lista} y la lista sin duplicados es ${valoresUnicos}`;
+}
+console.log(arraySinDuplicados(duplicados));
