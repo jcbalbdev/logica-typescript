855f9d8d799f2c7a04e8752ee7a60bdada09d473
jcbalbdev <jcbalbdev@gmail.com>
2025-04-27 17:07:29 -0500

    Actualización automática: 2025-04-27 17:07:29

--- Ejercicios/lv5-suma-total-con-reduce.js
+++ Ejercicios/lv5-suma-total-con-reduce.js
@@ -1,3 +1,9 @@
 "use strict";
 /* Usa reduce para sumar todos los elementos de un array numérico. */
 let listaNum = [1, 2, 3, 4, 1, 5, 7, 9, 10];
+function sumaConAcumulador(lista) {
+    let sumaDeElementos = lista.reduce((acc, numero) => {
+        return acc + numero;
+    }, 0);
+    return `la suma de esta lista (${lista}) es : ${sumaDeElementos} `;
+}
--- Ejercicios/lv5-suma-total-con-reduce.ts
+++ Ejercicios/lv5-suma-total-con-reduce.ts
@@ -1,4 +1,10 @@
 /* Usa reduce para sumar todos los elementos de un array numérico. */
 let listaNum: number[] = [1,2,3,4,1,5,7,9,10];
 
+function sumaConAcumulador(lista: number[]): string{
+  let sumaDeElementos: number = lista.reduce((acc,numero)=>{
+    return acc+numero; 
+  },0);
+  return `la suma de esta lista (${lista}) es : ${sumaDeElementos} `; 
+}
 
