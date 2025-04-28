f8dd79ad37cb1a56aed3f95ea24c4fd26f93f57f
jcbalbdev <jcbalbdev@gmail.com>
2025-04-27 21:25:55 -0500

    Actualización automática: 2025-04-27 21:25:55

--- Ejercicios/lv2-funcion-que-verifica-si-un-numero-es-par.js
+++ Ejercicios/lv2-funcion-que-verifica-si-un-numero-es-par.js
@@ -1,2 +1,6 @@
 "use strict";
 /* Usa una función que devuelva true si un número es par, false si no. */
+function numEsPar(numero) {
+    return numero % 2 === 0 ? true : false;
+}
+console.log(numEsPar(2));
--- Ejercicios/lv2-funcion-que-verifica-si-un-numero-es-par.ts
+++ Ejercicios/lv2-funcion-que-verifica-si-un-numero-es-par.ts
@@ -1,3 +1,7 @@
 /* Usa una función que devuelva true si un número es par, false si no. */
 
+function numEsPar(numero: number): boolean{
+  return numero%2===0?true:false;
+}
 
+console.log(numEsPar(2));
