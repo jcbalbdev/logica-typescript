ccf619d27c5439b4577e17b59ce45dac25f9fd41
jcbalbdev <jcbalbdev@gmail.com>
2025-04-27 19:17:22 -0500

    Actualización automática: 2025-04-27 19:17:22

--- Ejercicios/lv4-ordenar-un-array-de-numeros.ts
+++ Ejercicios/lv4-ordenar-un-array-de-numeros.ts
@@ -1,3 +1,13 @@
 /* Usa .sort() para ordenar de menor a mayor y viceversa. */
 
 let listaDeNumeros: number[] = [1,2,3,4,1,2,3,410]
+
+function ordenarNumeros(lista: number[]): string{
+  let listaOriginal: number[] = [...lista];
+
+  let listaOrdenada: number[] = lista.sort((a,b) => a - b);
+
+  return `la lista de palabras es ${listaOriginal} y la lista ordenada por tamaño de palabra es ${listaOrdenada}`;
+}
+
+console.log(ordenarNumeros(listaDeNumeros));
