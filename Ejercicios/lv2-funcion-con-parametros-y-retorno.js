14ed0fd03b740a327f65e7f4c958ff7b0ee3834d
jcbalbdev <jcbalbdev@gmail.com>
2025-04-27 20:51:20 -0500

    Actualización automática: 2025-04-27 20:51:20

--- Ejercicios/lv2-funcion-con-parametros-y-retorno.js
+++ Ejercicios/lv2-funcion-con-parametros-y-retorno.js
@@ -1,1 +1,6 @@
 "use strict";
+/* Crea una función miSuma(a, b) que retorne la suma de dos números. */
+function miSuma(a, b) {
+    return `la suma de ${a} y ${b} es : ${a + b}`;
+}
+console.log(miSuma(2, 3));
--- Ejercicios/lv2-funcion-con-parametros-y-retorno.ts
+++ Ejercicios/lv2-funcion-con-parametros-y-retorno.ts
@@ -1,1 +1,9 @@
+/* Crea una función miSuma(a, b) que retorne la suma de dos números. */
+
+function miSuma(a: number,b: number): string{
+  return `la suma de ${a} y ${b} es : ${a + b}`;
+}
+
+console.log(miSuma(2,3));
+
 
