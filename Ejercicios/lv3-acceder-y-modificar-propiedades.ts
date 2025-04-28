b781f1a9fc910bf5eeecfa87fba147ae46ae6b57
jcbalbdev <jcbalbdev@gmail.com>
2025-04-27 21:18:46 -0500

    Actualización automática: 2025-04-27 21:18:46

--- Ejercicios/lv3-acceder-y-modificar-propiedades.ts
+++ Ejercicios/lv3-acceder-y-modificar-propiedades.ts
@@ -2,20 +2,20 @@
 
 interface SuperHeroe {
   nombre:string;
-  "equipo de superhéroess"?:string;
+  "equipo de superhéroes"?:string;
   poder:string;
 }
 
 let flash: SuperHeroe = {
   nombre: "Barry Allen",
-  "equipo de superherores" : "liga de la justicia",
+  "equipo de superhéroes" : "liga de la justicia",
   poder:  "velocidad"
 }
 
-console.log(`el nombre del super heroe es ${flash.nombre} y pertenece a la ${flash["equipo de superherores"]}`);
+console.log(`el nombre del super heroe es ${flash.nombre} y pertenece a la ${flash["equipo de superhéroes"]}`);
 
 flash.poder = "super velocidad";
 console.log(`su super poder es ${flash.poder}`);
 
-delete flash["equipo de superherores"];
+delete flash["equipo de superhéroes"];
 console.log(flash);
