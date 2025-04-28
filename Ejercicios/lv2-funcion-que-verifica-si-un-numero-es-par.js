8a12a67414f17b0404a29efd598959b0f7032db6
jcbalbdev <jcbalbdev@gmail.com>
2025-04-27 21:22:53 -0500

    Actualización automática: 2025-04-27 21:22:53

---
+++ Ejercicios/lv2-funcion-que-verifica-si-un-numero-es-par.js
@@ -0,0 +1,2 @@
+"use strict";
+/* Usa una función que devuelva true si un número es par, false si no. */
---
+++ Ejercicios/lv2-funcion-que-verifica-si-un-numero-es-par.ts
@@ -0,0 +1,3 @@
+/* Usa una función que devuelva true si un número es par, false si no. */
+
+
