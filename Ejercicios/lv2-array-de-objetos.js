3f598fa2aa5a38e31f1090177a9fb9da6521518e
jcbalbdev <jcbalbdev@gmail.com>
2025-04-27 22:11:21 -0500

    Actualización automática: 2025-04-27 22:11:21

--- Ejercicios/lv2-array-de-objetos.js
+++ Ejercicios/lv2-array-de-objetos.js
@@ -1,2 +1,19 @@
 "use strict";
 /* Crea una lista de usuarios, donde cada usuario tenga nombre, email y edad */
+let listaUsuarios = [
+    {
+        nombre: "Barry",
+        email: "barry@gmail.com",
+        edad: 12
+    },
+    {
+        nombre: "Diana",
+        email: "diana@gmail.com",
+        edad: 23
+    },
+    {
+        nombre: "Bruce",
+        email: "soybatman@gmail.com",
+        edad: 32
+    }
+];
--- Ejercicios/lv2-array-de-objetos.ts
+++ Ejercicios/lv2-array-de-objetos.ts
@@ -1,3 +1,25 @@
 /* Crea una lista de usuarios, donde cada usuario tenga nombre, email y edad */
 
+interface Usuarios {
+  nombre: string;
+  email:string;
+  edad:number;
+}
 
+let listaUsuarios: Usuarios[] = [
+  {
+    nombre:"Barry",
+    email:"barry@gmail.com",
+    edad:12
+  },
+  {
+    nombre:"Diana",
+    email:"diana@gmail.com",
+    edad:23
+  },
+  {
+    nombre:"Bruce",
+    email:"soybatman@gmail.com",
+    edad:32
+  }
+]
