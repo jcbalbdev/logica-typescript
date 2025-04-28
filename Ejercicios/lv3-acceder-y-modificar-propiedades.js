3f2db3c4688999af438f9bd497b9de47dbfa0f79
jcbalbdev <jcbalbdev@gmail.com>
2025-04-27 21:03:29 -0500

    Actualización automática: 2025-04-27 21:03:29

--- Ejercicios/lv3-acceder-y-modificar-propiedades.js
+++ Ejercicios/lv3-acceder-y-modificar-propiedades.js
@@ -1,5 +1,7 @@
 "use strict";
 /* Accede a propiedades de un objeto, modifica una y elimina otra. */
 let flash = {
-    nombre: ""
+    nombre: "Barry Allen",
+    equipo: "liga de la justicia",
+    poder: "super velocidad"
 };
--- Ejercicios/lv3-acceder-y-modificar-propiedades.ts
+++ Ejercicios/lv3-acceder-y-modificar-propiedades.ts
@@ -1,11 +1,13 @@
 /* Accede a propiedades de un objeto, modifica una y elimina otra. */
 
 interface SuperHeroe {
-  nombre:string,
-  equipo:string,
-  poder:string,
+  nombre:string;
+  equipo:string;
+  poder:string;
 }
 
 let flash: SuperHeroe = {
-  nombre: ""
+  nombre: "Barry Allen",
+  equipo : "liga de la justicia",
+  poder:  "super velocidad"
 }
