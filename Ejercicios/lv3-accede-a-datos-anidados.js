90432db6cf87bcefcfd2be329e3765f56358abb6
jcbalbdev <jcbalbdev@gmail.com>
2025-04-27 22:16:28 -0500

    Actualización automática: 2025-04-27 22:16:28

--- Ejercicios/lv3-accede-a-datos-anidados.js
+++ Ejercicios/lv3-accede-a-datos-anidados.js
@@ -1,1 +1,18 @@
 "use strict";
+let miListaUsuarios = [
+    {
+        nombre: "Barry",
+        email: "barry@gmail.com",
+        edad: 12
+    },
+    {
+        nombre: "Diana",
+        email: "diana@gmail.com",
+        edad: 23
+    },
+    {
+        nombre: "Bruce",
+        email: "soybatman@gmail.com",
+        edad: 32
+    }
+];
--- Ejercicios/lv3-accede-a-datos-anidados.ts
+++ Ejercicios/lv3-accede-a-datos-anidados.ts
@@ -1,1 +1,24 @@
+/* Accede al email del segundo usuario de un array de objetos. */
+interface MisUsuarios {
+  nombre: string;
+  email:string;
+  edad:number;
+}
 
+let miListaUsuarios: MisUsuarios[] = [
+  {
+    nombre:"Barry",
+    email:"barry@gmail.com",
+    edad:12
+  },
+  {
+    nombre:"Diana",
+    email:"diana@gmail.com",
+    edad:23
+  },
+  {
+    nombre:"Bruce",
+    email:"soybatman@gmail.com",
+    edad:32
+  }
+]
