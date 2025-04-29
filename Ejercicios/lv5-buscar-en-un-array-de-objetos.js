704bf0eff3398ea63b1514f9759dd15318308208
jcbalbdev <jcbalbdev@gmail.com>
2025-04-29 18:41:48 -0500

    Actualización automática: 2025-04-29 18:41:48

--- Ejercicios/lv5-buscar-en-un-array-de-objetos.js
+++ Ejercicios/lv5-buscar-en-un-array-de-objetos.js
@@ -1,2 +1,22 @@
 "use strict";
 /* Usa find para encontrar el producto con mayor precio en una lista. */
+let listaDeProductos = [
+    {
+        nombre: "mesa gamer",
+        precio: 48
+    },
+    {
+        nombre: "silla gamer",
+        precio: 36
+    },
+    {
+        nombre: "mouse gamer",
+        precio: 56
+    },
+    {
+        nombre: "teclado gamer",
+        precio: 34
+    }
+];
+let mayorPrecio = listaDeProductos.find((producto) => producto.precio > 50);
+console.log(mayorPrecio);
--- Ejercicios/lv5-buscar-en-un-array-de-objetos.ts
+++ Ejercicios/lv5-buscar-en-un-array-de-objetos.ts
@@ -1,1 +1,30 @@
 /* Usa find para encontrar el producto con mayor precio en una lista. */
+
+interface Item{
+  nombre:string;
+  precio:number;
+}
+
+let listaDeProductos: Item[] = [
+  {
+    nombre:"mesa gamer",
+    precio:48
+  },
+  {
+    nombre:"silla gamer",
+    precio:36
+  },
+  {
+    nombre:"mouse gamer",
+    precio:56
+  },
+  {
+    nombre:"teclado gamer",
+    precio:34
+  }
+]
+
+let mayorPrecio = listaDeProductos.find((producto)=>producto.precio>50);
+
+
+console.log(mayorPrecio);
