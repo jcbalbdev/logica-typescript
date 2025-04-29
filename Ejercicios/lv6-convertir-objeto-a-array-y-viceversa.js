f78e550cc895af1a03dd7ed67c99c24f44b6d1cc
jcbalbdev <jcbalbdev@gmail.com>
2025-04-29 15:49:13 -0500

    Actualización automática: 2025-04-29 15:49:13

--- Ejercicios/lv6-convertir-objeto-a-array-y-viceversa.js
+++ Ejercicios/lv6-convertir-objeto-a-array-y-viceversa.js
@@ -1,2 +1,6 @@
 "use strict";
 /* Convierte un objeto a array y viceversa . Usa Object.entries, Object.fromEntries, Object.keys y Object.values. */
+let pikachu = {
+    nombre: "pikachu",
+    tipo: "electrico"
+};
--- Ejercicios/lv6-convertir-objeto-a-array-y-viceversa.ts
+++ Ejercicios/lv6-convertir-objeto-a-array-y-viceversa.ts
@@ -1,1 +1,13 @@
 /* Convierte un objeto a array y viceversa . Usa Object.entries, Object.fromEntries, Object.keys y Object.values. */
+
+interface Pokemon{
+  nombre:string;
+  tipo:string;
+}
+
+let pikachu: Pokemon = {
+  nombre: "pikachu",
+  tipo:"electrico"
+}
+
+
