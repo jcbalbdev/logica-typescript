f42599cc579f25ff09a43bc3fc09f30ed94619e8
jcbalbdev <jcbalbdev@gmail.com>
2025-04-29 17:56:22 -0500

    Actualización automática: 2025-04-29 17:56:22

--- Ejercicios/lv5-lista-de-objetos-filtrar-por-propiedad.js
+++ Ejercicios/lv5-lista-de-objetos-filtrar-por-propiedad.js
@@ -1,2 +1,16 @@
 "use strict";
-/* Dada una lista de usuarios con nombre y edad, filtra los mayores de 18. */
+/* Dada una lista de personas con nombre y edad, filtra los mayores de 18. */
+let listaPersonas = [
+    {
+        nombre: "pedro",
+        edad: 32
+    },
+    {
+        nombre: "bruce",
+        edad: 12
+    },
+    {
+        nombre: "barry",
+        edad: 18
+    }
+];
--- Ejercicios/lv5-lista-de-objetos-filtrar-por-propiedad.ts
+++ Ejercicios/lv5-lista-de-objetos-filtrar-por-propiedad.ts
@@ -1,1 +1,21 @@
-/* Dada una lista de usuarios con nombre y edad, filtra los mayores de 18. */
+/* Dada una lista de personas con nombre y edad, filtra los mayores de 18. */
+
+interface DatosPersona{
+  nombre:string;
+  edad:number;
+}
+
+let listaPersonas: DatosPersona[] = [
+  {
+    nombre:"pedro",
+    edad:32
+  },
+  {
+    nombre:"bruce",
+    edad:12
+  },
+  {
+    nombre:"barry",
+    edad:18
+  }
+]
