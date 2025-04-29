2e886986cbb05df6278c6efd614712b8715fbc0f
jcbalbdev <jcbalbdev@gmail.com>
2025-04-29 15:47:11 -0500

    Actualización automática: 2025-04-29 15:47:11

--- Ejercicios/lv6-convertir-objeto-a-array-y-viceversa.js
+++ Ejercicios/lv6-convertir-objeto-a-array-y-viceversa.js
@@ -1,1 +1,2 @@
 "use strict";
+/* Convierte un objeto a array y viceversa . Usa Object.entries, Object.fromEntries, Object.keys y Object.values. */
--- Ejercicios/lv6-convertir-objeto-a-array-y-viceversa.ts
+++ Ejercicios/lv6-convertir-objeto-a-array-y-viceversa.ts
@@ -1,1 +1,1 @@
-
+/* Convierte un objeto a array y viceversa . Usa Object.entries, Object.fromEntries, Object.keys y Object.values. */
