051b1e14fba8e93b970be2de30406d103ca04be4
jcbalbdev <jcbalbdev@gmail.com>
2025-04-29 18:09:36 -0500

    Actualización automática: 2025-04-29 18:09:36

--- Ejercicios/lv3-reutilizacion-de-calculos-matematicos.js
+++ Ejercicios/lv3-reutilizacion-de-calculos-matematicos.js
@@ -1,1 +1,17 @@
 "use strict";
+/* Crea funciones areaRectangulo, areaTriangulo, areaCirculo. Usa cada una con diferentes valores. */
+function producto(a, b) {
+    return a * b;
+}
+function areaRectangulo(a, b) {
+    return producto(a, b);
+}
+function areaTriangulo(a, b) {
+    return producto(a, b) / 2;
+}
+function areaCirculo(a, b) {
+    return producto(a, b);
+}
+console.log(`el area de un rectangulo de lados 4 y 5 es : ${areaRectangulo(4, 5)}`);
+console.log(`el area de un triangulo de altura y base 2 y 5 es : ${areaRectangulo(2, 5)}`);
+console.log(`el area de un cuadrado de lados 6 y 5 es : ${areaRectangulo(6, 5)}`);
