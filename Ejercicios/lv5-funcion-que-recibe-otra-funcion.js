cd7575bba9dd3548e4187be52d2f25bca8870e64
jcbalbdev <jcbalbdev@gmail.com>
2025-04-29 18:38:44 -0500

    Actualización automática: 2025-04-29 18:38:44

--- Ejercicios/lv5-funcion-que-recibe-otra-funcion.js
+++ Ejercicios/lv5-funcion-que-recibe-otra-funcion.js
@@ -1,1 +1,12 @@
 "use strict";
+/* Escribe una función ejecutar que reciba otra función como parámetro y la ejecute. */
+function suma(a, b) {
+    return a + b;
+}
+function resta(a, b) {
+    return a - b;
+}
+function ejecutar(a, b, operacion) {
+    return operacion(a, b);
+}
+console.log(ejecutar(2, 3, suma));
--- Ejercicios/lv5-funcion-que-recibe-otra-funcion.ts
+++ Ejercicios/lv5-funcion-que-recibe-otra-funcion.ts
@@ -1,1 +1,15 @@
+/* Escribe una función ejecutar que reciba otra función como parámetro y la ejecute. */
 
+function suma(a: number,b: number){
+  return a + b;
+}
+
+function resta(a: number,b: number){
+  return a - b;
+}
+
+function ejecutar(a: number,b: number,operacion : (x: number,y: number) => number): number{
+  return operacion(a,b);
+}
+
+console.log(ejecutar(2,3,suma));
