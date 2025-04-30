05c9f408b9a2c0eff141e6cf8748a0aecea976c7
jcbalbdev <jcbalbdev@gmail.com>
2025-04-29 19:03:02 -0500

    Actualización automática: 2025-04-29 19:03:02

--- Ejercicios/lv4-funciones-puras-vs-funciones-con-efecto.js
+++ Ejercicios/lv4-funciones-puras-vs-funciones-con-efecto.js
@@ -1,1 +1,9 @@
 "use strict";
+/* Escribe una función pura que devuelva el doble de un número, y otra que también lo imprima (efecto colateral). */
+function doble(a) {
+    return a * a;
+}
+function imprimiendoDoble(a) {
+    return `el doble de ${a}  es ${doble(a)}`;
+}
+console.log(imprimiendoDoble(3));
--- Ejercicios/lv4-funciones-puras-vs-funciones-con-efecto.ts
+++ Ejercicios/lv4-funciones-puras-vs-funciones-con-efecto.ts
@@ -1,1 +1,11 @@
+/* Escribe una función pura que devuelva el doble de un número, y otra que también lo imprima (efecto colateral). */
 
+function doble(a: number): number{
+  return a*a;
+}
+
+function imprimiendoDoble(a: number): string{
+  return `el doble de ${a}  es ${doble(a)}`;
+}
+
+console.log(imprimiendoDoble(3));
